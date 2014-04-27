2f9c04e31596ec34ad3986cdd3e726f9fb865254
Shengwen Cheng <shengwen1997.tw@gmail.com>
2014-04-27 22:23:27 +0800

    Remove the old binary transmit functions

--- program/system/communication.c
+++ program/system/communication.c
@@ -11,19 +11,6 @@ extern xTaskHandle ground_station_handle;
 mavlink_message_t received_msg;
 mavlink_status_t received_status;
 
-void generate_package(IMU_package *package, uint8_t *buf)
-{
-	memcpy(&buf[0], &((package->roll)), sizeof(int16_t));
-	memcpy(&buf[2], &((package->pitch)), sizeof(int16_t));
-	memcpy(&buf[4], &((package->yaw)), sizeof(int16_t));
-	memcpy(&buf[6], &((package->acc_x)), sizeof(int16_t));
-	memcpy(&buf[8], &((package->acc_y)), sizeof(int16_t));
-	memcpy(&buf[10], &((package->acc_z)), sizeof(int16_t));
-	memcpy(&buf[12], &((package->gyro_x)), sizeof(int16_t));
-	memcpy(&buf[14], &((package->gyro_y)), sizeof(int16_t));
-	memcpy(&buf[16], &((package->gyro_z)), sizeof(int16_t));
-}
-
 void send_package(uint8_t *buf, size_t size)
 {
 	int i;
