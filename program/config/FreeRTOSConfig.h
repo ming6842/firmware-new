8488de2e85a5642a0c1d6380e8fe296d8f0e6019
Shengwen Cheng <shengwen1997.tw@gmail.com>
2014-05-16 23:35:07 +0800

    Fix the timer not working issue by increasing the timer priority

--- program/config/FreeRTOSConfig.h
+++ program/config/FreeRTOSConfig.h
@@ -105,7 +105,7 @@
 
 /* Timer definitions. */
 #define configUSE_TIMERS 1
-#define configTIMER_TASK_PRIORITY 1
+#define configTIMER_TASK_PRIORITY (tskIDLE_PRIORITY + 5) 
 #define configTIMER_QUEUE_LENGTH 10
 #define configTIMER_TASK_STACK_DEPTH configMINIMAL_STACK_SIZE
 
