aadc62a0cdf5edd595c4a64ff721bfe365d6d12c
Shengwen Cheng <shengwen1997.tw@gmail.com>
2014-05-18 22:35:18 +0800

    Create the variable management mechanism prototype

--- program/system/global.c
+++ program/system/global.c
@@ -57,3 +57,60 @@ status_t SD_status;
 
 uint64_t boot_time = 0;
 
+void reset_vehicle_data(int index, char *name, AccessRight access_right)
+{
+}
+
+void set_vehicle_data(int index, int int_val, float flt_val, Type type)
+{
+}
+
+#define QUADCOPTER 0
+void init_vehicle_data()
+{
+	/* Vehicle information */
+	reset_vehicle_data(VEHICLE_TYPE, "vehicle_type", READ_ONLY);
+	set_vehicle_data(VEHICLE_TYPE, QUADCOPTER, 0.0, INTEGER);
+
+	/* Boot ime */
+	reset_vehicle_data(BOOT_TIME, "boot_time", READ_ONLY);
+	set_vehicle_data(BOOT_TIME, 0, 0.0, INTEGER);
+
+	reset_vehicle_data(PWM1_CCR, "pwm1", READ_ONLY);
+	reset_vehicle_data(PWM2_CCR, "pwm2", READ_ONLY);
+	reset_vehicle_data(PWM3_CCR, "pwm3", READ_ONLY);
+	reset_vehicle_data(PWM4_CCR, "pwm4", READ_ONLY);
+	reset_vehicle_data(PWM5_CCR, "pwm5", READ_ONLY);
+
+	reset_vehicle_data(MOTOR1, "motor1", READ_ONLY);
+	reset_vehicle_data(MOTOR2, "motor2", READ_ONLY);
+	reset_vehicle_data(MOTOR3, "motor3", READ_ONLY);
+	reset_vehicle_data(MOTOR4, "motor4", READ_ONLY);
+
+	reset_vehicle_data(NO_RC_SIGNAL_MSG, "rc.status", READ_ONLY);
+	
+	reset_vehicle_data(RC_EXP_ROLL, "rc.roll", READ_ONLY);
+	reset_vehicle_data(RC_EXP_PITCH, "rc.pitch", READ_ONLY);
+	reset_vehicle_data(RC_EXP_YAW, "rc.yaw", READ_ONLY);
+	reset_vehicle_data(RC_EXP_THR, "rc.throttle", READ_ONLY);
+
+	/* IMU information */
+	reset_vehicle_data(TRUE_ROLL, "imu.roll", READ_ONLY);
+	reset_vehicle_data(TRUE_PITCH, "imu.pitch", READ_ONLY);
+	reset_vehicle_data(TRUE_YAW, "imu.yaw", READ_ONLY);
+
+	reset_vehicle_data(PID_ROLL, "pid.roll", READ_ONLY);
+        reset_vehicle_data(PID_PITCH, "pid.pitch", READ_ONLY);
+        reset_vehicle_data(PID_YAW, "pid.yaw", READ_ONLY);
+
+	/* GPS Location */
+        reset_vehicle_data(GPS_LAT, "gps.latitude", READ_ONLY);
+        reset_vehicle_data(GPS_LON, "gps.longitude", READ_ONLY);
+        reset_vehicle_data(GPS_ALT, "gps.altitude", READ_ONLY);
+
+	/* GPS Speed */
+        reset_vehicle_data(GPS_VX, "gps.vx", READ_ONLY);
+        reset_vehicle_data(GPS_VY, "gps.vy", READ_ONLY);
+        reset_vehicle_data(GPS_VZ, "gps.vz", READ_ONLY);
+} 
+
