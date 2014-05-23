98d1f5f636b24e04c600c18b6a09cf7c5fdfc0df
fboris <fantasyboris@gmail.com>
2014-05-23 20:36:55 +0800

    Write unit testing code for r/c command parsing
    Former-commit-id: a32208424745597238dcd2547c035ae714b50d54 [formerly 828cfab1ed423379c9d9be1408bb912908ae2cb1]
    Former-commit-id: 22d8e553b1faeff0b35361d05ce86b6b01bf33eb

---
+++ program/test/test.c
@@ -0,0 +1,86 @@
+#include "QuadCopterConfig.h"
+#include "test.h"
+/*
+
+*/
+static void test_Update_RC_Control()
+{
+
+	int16_t Roll, Pitch, Yaw, Thr; 
+	uint8_t safety;
+	
+	//test the condition out of maximum value
+	system.variable[PWM1_CCR].value = MAX_PWM1_INPUT + 200;
+	system.variable[PWM2_CCR].value = MAX_PWM2_INPUT + 200;
+	system.variable[PWM3_CCR].value = MAX_PWM3_INPUT + 200;
+	system.variable[PWM4_CCR].value = MAX_PWM4_INPUT + 200;
+	system.variable[PWM5_CCR].value = MAX_PWM5_INPUT + 200;
+
+	Update_RC_Control( &Roll, &Pitch, &Yaw, &Thr, &safety);
+	
+	TEST_ASSERT(Roll >= MAX_CTRL_ROLL);
+	TEST_ASSERT(Pitch >= MAX_CTRL_PITCH);
+	TEST_ASSERT(Yaw >= MAX_CTRL_YAW);
+	TEST_ASSERT(Thr == PWM_MOTOR_MAX);
+	TEST_ASSERT(safety == ENGINE_OFF);
+
+	//test the condition in normal 
+	system.variable[PWM1_CCR].value = MAX_PWM1_INPUT - 200;
+	system.variable[PWM2_CCR].value = MAX_PWM2_INPUT - 200;
+	system.variable[PWM3_CCR].value = MAX_PWM3_INPUT - 200;
+	system.variable[PWM4_CCR].value = MAX_PWM4_INPUT - 200;
+	system.variable[PWM5_CCR].value = MAX_PWM5_INPUT - 200;
+
+	Update_RC_Control( &Roll, &Pitch, &Yaw, &Thr, &safety);
+	TEST_ASSERT( ( Roll >= MIN_CTRL_ROLL) && (Roll <= MAX_CTRL_ROLL) );
+	TEST_ASSERT( ( Pitch >= MIN_CTRL_PITCH) && (Pitch <= MAX_CTRL_PITCH) );
+	TEST_ASSERT( ( Yaw >= MIN_CTRL_YAW) && (Yaw <= MAX_CTRL_YAW) );
+	TEST_ASSERT( ( Thr >= PWM_MOTOR_MIN) && (Thr <= PWM_MOTOR_MAX) );
+	TEST_ASSERT( safety == ENGINE_OFF );
+
+	////test the condition: close to minimun value
+	system.variable[PWM1_CCR].value = MIN_PWM1_INPUT + 200;
+	system.variable[PWM2_CCR].value = MIN_PWM2_INPUT + 200;
+	system.variable[PWM3_CCR].value = MIN_PWM3_INPUT + 200;
+	system.variable[PWM4_CCR].value = MIN_PWM4_INPUT + 200;
+	system.variable[PWM5_CCR].value = MIN_PWM5_INPUT + 200;
+
+	Update_RC_Control( &Roll, &Pitch, &Yaw, &Thr, &safety);
+	TEST_ASSERT( ( Roll >= MIN_CTRL_ROLL) && (Roll <= MAX_CTRL_ROLL) );
+	TEST_ASSERT( ( Pitch >= MIN_CTRL_PITCH) && (Pitch <= MAX_CTRL_PITCH) );
+	TEST_ASSERT( ( Yaw >= MIN_CTRL_YAW) && (Yaw <= MAX_CTRL_YAW) );
+	TEST_ASSERT( ( Thr >= PWM_MOTOR_MIN) && (Thr <= PWM_MOTOR_MAX) );
+	TEST_ASSERT( safety == ENGINE_ON );
+
+	////test the condition: lower to minimun value
+	system.variable[PWM1_CCR].value = MIN_PWM1_INPUT - 200;
+	system.variable[PWM2_CCR].value = MIN_PWM2_INPUT - 200;
+	system.variable[PWM3_CCR].value = MIN_PWM3_INPUT - 200;
+	system.variable[PWM4_CCR].value = MIN_PWM4_INPUT - 200;
+	system.variable[PWM5_CCR].value = MIN_PWM5_INPUT - 200;
+
+	Update_RC_Control( &Roll, &Pitch, &Yaw, &Thr, &safety);
+	TEST_ASSERT( Roll <= MIN_CTRL_ROLL );
+	TEST_ASSERT( Pitch <= MIN_CTRL_PITCH );
+	TEST_ASSERT( Yaw <= MIN_CTRL_YAW );
+	TEST_ASSERT( Thr == PWM_MOTOR_MIN );
+	TEST_ASSERT( safety == ENGINE_ON );
+	test_pass_count++;
+}
+void run_tests()
+{
+	test_Update_RC_Control();
+	printf("\r\nPass:%d,Failed:%d\r\n", test_pass_count, test_failed_count);
+}
+int main()
+{	
+	test_pass_count=0;
+	test_failed_count=0;
+	run_tests();
+	if (test_failed_count ==0) {
+		return 0;
+	} else {
+
+		return 1;
+	}
+}
---
+++ program/test/test.h
@@ -0,0 +1,15 @@
+#ifndef __TEST_H
+#define __TEST_H
+//#define TEST_ASSERT(message, test) do { if (!(test)) return message; } while (0)
+// #define PASS 0
+// #define FAILED -1
+int16_t test_pass_count, test_failed_count;
+
+#define TEST_ASSERT(test) do { if (!(test)){ test_failed_count++; \
+				printf("TEST FAILED!:in file:%s, at line %d", __FILE__, __LINE__); \
+				return; \
+			} \
+			} while (0)
+#define mu_run_test(test) do { char *message = test(); tests_run++; \
+                                if (message) return message; } while (0)
+#endif
