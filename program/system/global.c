f782b3f47376ec70cc69b2aa3b71d7806cb56080
Shengwen Cheng <shengwen1997.tw@gmail.com>
2014-05-25 10:00:26 +0800

    Global: Add some doxygen comment

--- program/system/global.c
+++ program/system/global.c
@@ -61,11 +61,12 @@ void init_global_data()
         reset_global_data(GPS_VZ, "gps.vz", READ_ONLY);
 } 
 
-int get_global_data_count()
-{
-	return SYS_VAR_CNT;
-}
 
+/**
+  * @brief  reset data value and set global data's name and assess right
+  * @param  index (int), name (char *), assess_right (AcessRight)
+  * @retval None
+  */
 int reset_global_data(int index, char *name, AccessRight access_right)
 {
 	ASSERT((index >= 0) && (index < SYS_VAR_CNT)); /* Index is in the range or not */
@@ -81,52 +82,103 @@ int reset_global_data(int index, char *name, AccessRight access_right)
 	return 0;
 }
 
+/**
+  * @brief  get the count of global data
+  * @param  None
+  * @retval global data count (int)
+  */
+int get_global_data_count()
+{
+	return SYS_VAR_CNT;
+}
+
+/**
+  * @brief  set global data's value (int version)
+  * @param  index (int), value (int)
+  * @retval None
+  */
 void set_global_data_int(int index, int value)
 {
-	ASSERT((index >= 0) && (index < SYS_VAR_CNT)); /* Index is in the range or not */
+	/* Index is in the range or not */
+	ASSERT((index >= 0) && (index < SYS_VAR_CNT));
 
 	/* Set the variable type and value */
 	global_mav_data_list[index].type = INTEGER;
 	global_mav_data_list[index].int_value = value;
 }
 
+/**
+  * @brief  set global data's value (float version)
+  * @param  index (int), value (int)
+  * @retval None
+  */
 void set_global_data_float(int index, float value)
 {
-	ASSERT((index >= 0) && (index < SYS_VAR_CNT)); /* Index is in the range or not */
+	/* Index is in the range or not */
+	ASSERT((index >= 0) && (index < SYS_VAR_CNT));
 
 	/* Set the variable type and value */
 	global_mav_data_list[index].type = FLOAT;
 	global_mav_data_list[index].flt_value = value;	
 }
 
+/**
+  * @brief  get the variable type of global data
+  * @param  index (int)
+  * @retval variable type (Type)
+  */
 Type get_global_data_type(int index)
 {
-	ASSERT((index >= 0) && (index < SYS_VAR_CNT)); /* Index is in the range or not */
+        /* Index is in the range or not */
+	ASSERT((index >= 0) && (index < SYS_VAR_CNT));
 
 	return global_mav_data_list[index].type;
 }
 
+/**
+  * @brief  get the name of global data
+  * @param  index (int)
+  * @retval global data's name (char *)
+  */
 char *read_global_data_name(int index)
 {
-	ASSERT((index >= 0) && (index < SYS_VAR_CNT)); /* Index is in the range or not */
+        /* Index is in the range or not */
+	ASSERT((index >= 0) && (index < SYS_VAR_CNT));
 	
 	return global_mav_data_list[index].name;
 }
 
+/**
+  * @brief  get the value of global data (int version)
+  * @param  index (int)
+  * @retval global data's value (int)
+  */
 int read_global_data_int(int index)
 {
-	ASSERT((index >= 0) && (index < SYS_VAR_CNT)); /* Index is in the range or not */
+        /* Index is in the range or not */
+	ASSERT((index >= 0) && (index < SYS_VAR_CNT));
 	
 	return global_mav_data_list[index].int_value;
 }
 
+/**
+  * @brief  get the value of global data (float version)
+  * @param  index (float)
+  * @retval global data's value (float)
+  */
 float read_global_data_float(int index)
 {
-	ASSERT((index >= 0) && (index < SYS_VAR_CNT)); /* Index is in the range or not */
+        /* Index is in the range or not */
+	ASSERT((index >= 0) && (index < SYS_VAR_CNT));
 
 	return global_mav_data_list[index].flt_value;
 }
 
+/**
+  * @brief  get the boot time value
+  * @param  None
+  * @retval boot time value (int)
+  */
 uint32_t get_boot_time()
 {
 	return read_global_data_int(BOOT_TIME);
