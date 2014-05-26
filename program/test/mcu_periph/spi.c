89fc17255cd1500c3a868861135cb40bb79791c0
fboris <fantasyboris@gmail.com>
2014-05-26 20:49:16 +0800

    Finish spi with MPU9250 and pwm output
    Former-commit-id: f2580ae910cec2924e7e84107c35c1ca898675ef [formerly ca91a26abba4a9d57b7657eee46b2b4a769bac60]
    Former-commit-id: 64dfbd0367de46cc1fc56db3684788ddc430c4eb

--- program/test/mcu_periph/spi.c
+++ program/test/mcu_periph/spi.c
@@ -46,27 +46,32 @@ void enable_spi1()
 
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_SPI1, ENABLE);
 
-	GPIO_PinAFConfig(GPIOA, GPIO_PinSource4, GPIO_AF_SPI1);
 	GPIO_PinAFConfig(GPIOA, GPIO_PinSource5, GPIO_AF_SPI1);
 	GPIO_PinAFConfig(GPIOA, GPIO_PinSource6, GPIO_AF_SPI1);
 	GPIO_PinAFConfig(GPIOA, GPIO_PinSource7, GPIO_AF_SPI1);
 	/* CSN PB12 */
-	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_4 | GPIO_Pin_5 |
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_5 |
 					GPIO_Pin_6 | GPIO_Pin_7;
 	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
 	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
 	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
 	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_Init(GPIOA, &GPIO_InitStruct);
-	
+
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_4;
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOA, &GPIO_InitStruct);
 
 	SPI_InitStruct.SPI_Direction = SPI_Direction_2Lines_FullDuplex; // 雙線全雙工
 	SPI_InitStruct.SPI_Mode = SPI_Mode_Master; // 主模式
 	SPI_InitStruct.SPI_DataSize = SPI_DataSize_8b; // 數據大小8位
-	SPI_InitStruct.SPI_CPOL = SPI_CPOL_Low; // 時鐘極性，空閒時為低
-	SPI_InitStruct.SPI_CPHA = SPI_CPHA_1Edge; // 第1個邊沿有效，上升沿為采樣時刻
+	SPI_InitStruct.SPI_CPOL = SPI_CPOL_High; // 時鐘極性，空閒時為低
+	SPI_InitStruct.SPI_CPHA = SPI_CPHA_2Edge; // 第1個邊沿有效，上升沿為采樣時刻
 	SPI_InitStruct.SPI_NSS = SPI_NSS_Soft; // NSS信號由軟件產生
-	SPI_InitStruct.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_2; // 8分頻，9MHz
+	SPI_InitStruct.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_256; // 8分頻，9MHz
 	SPI_InitStruct.SPI_FirstBit = SPI_FirstBit_MSB; // 高位在前
 	SPI_InitStruct.SPI_CRCPolynomial = 7;
 	SPI_Init(SPI1, &SPI_InitStruct);
@@ -118,11 +123,10 @@ void enable_spi4()
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_SPI4, ENABLE);
 
 	GPIO_PinAFConfig(GPIOE, GPIO_PinSource2, GPIO_AF_SPI4);
-	GPIO_PinAFConfig(GPIOE, GPIO_PinSource4, GPIO_AF_SPI4);
 	GPIO_PinAFConfig(GPIOE, GPIO_PinSource5, GPIO_AF_SPI4);
 	GPIO_PinAFConfig(GPIOE, GPIO_PinSource6, GPIO_AF_SPI4);
 	/* CSN PB12 */
-	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_2 | GPIO_Pin_4 |
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_2 | 
 					GPIO_Pin_5 | GPIO_Pin_6;
 	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
 	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
@@ -130,14 +134,17 @@ void enable_spi4()
 	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_Init(GPIOE, &GPIO_InitStruct);
 	
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_4;
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_Init(GPIOE, &GPIO_InitStruct);
 
 	SPI_InitStruct.SPI_Direction = SPI_Direction_2Lines_FullDuplex; // 雙線全雙工
 	SPI_InitStruct.SPI_Mode = SPI_Mode_Master; // 主模式
 	SPI_InitStruct.SPI_DataSize = SPI_DataSize_8b; // 數據大小8位
-	SPI_InitStruct.SPI_CPOL = SPI_CPOL_Low; // 時鐘極性，空閒時為低
-	SPI_InitStruct.SPI_CPHA = SPI_CPHA_1Edge; // 第1個邊沿有效，上升沿為采樣時刻
+	SPI_InitStruct.SPI_CPOL = SPI_CPOL_High; // 時鐘極性，空閒時為低
+	SPI_InitStruct.SPI_CPHA = SPI_CPHA_2Edge; // 第1個邊沿有效，上升沿為采樣時刻
 	SPI_InitStruct.SPI_NSS = SPI_NSS_Soft; // NSS信號由軟件產生
-	SPI_InitStruct.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_2; // 8分頻，9MHz
+	SPI_InitStruct.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_256; // 8分頻，9MHz
 	SPI_InitStruct.SPI_FirstBit = SPI_FirstBit_MSB; // 高位在前
 	SPI_InitStruct.SPI_CRCPolynomial = 7;
 	SPI_Init(SPI4, &SPI_InitStruct);
