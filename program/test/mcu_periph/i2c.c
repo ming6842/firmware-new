9de75fab3f66c24d331891bd8edab39ceba54ee4
fboris <fantasyboris@gmail.com>
2014-05-26 23:57:26 +0800

    Send device address through I2C1
    Former-commit-id: 584d51ad5b9bfc21596f77a6e4214413adca4abc [formerly 2ec03dba82e95726a1eeb10cbb7e3158f954fbda]
    Former-commit-id: 17233ae5e5f5605a3e323c09cff2b96655fcf7cd

---
+++ program/test/mcu_periph/i2c.c
@@ -0,0 +1,71 @@
+
+#include "stm32f4xx_conf.h"	
+#include "i2c.h"
+void enable_i2c1()
+{
+	GPIO_InitTypeDef GPIO_InitStruct;
+	NVIC_InitTypeDef NVIC_InitStruct;
+	I2C_InitTypeDef I2C_InitStruct;
+
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C1, ENABLE);
+	GPIO_PinAFConfig(GPIOB, GPIO_PinSource8, GPIO_AF_I2C1);
+	GPIO_PinAFConfig(GPIOB, GPIO_PinSource9, GPIO_AF_I2C1);
+
+	/* SCL I2Cx_SCL_PIN */  /* SDA I2Cx_SDA_PIN*/
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_9;
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStruct.GPIO_OType = GPIO_OType_OD;
+	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_InitStruct.GPIO_Pin = EEPROM_A2_PIN;
+	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+	GPIO_Init(EEPROM_A2_PIN_GROUP, &GPIO_InitStruct);
+	GPIO_SetBits(EEPROM_A2_PIN_GROUP, EEPROM_A2_PIN);
+
+	I2C_InitStruct.I2C_Mode = I2C_Mode_I2C;
+	I2C_InitStruct.I2C_DutyCycle = I2C_DutyCycle_2;
+	I2C_InitStruct.I2C_OwnAddress1 = 0x00;
+	I2C_InitStruct.I2C_Ack = I2C_Ack_Enable;
+	I2C_InitStruct.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit;
+	I2C_InitStruct.I2C_ClockSpeed =100000;
+	I2C_Cmd(I2C1, ENABLE);
+	I2C_Init(I2C1, &I2C_InitStruct);
+
+}
+
+void enable_i2c2()
+{
+	GPIO_InitTypeDef GPIO_InitStruct;
+	NVIC_InitTypeDef NVIC_InitStruct;
+	I2C_InitTypeDef I2C_InitStruct;
+
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C2, ENABLE);
+	GPIO_PinAFConfig(GPIOB, GPIO_PinSource10, GPIO_AF_I2C2);
+	GPIO_PinAFConfig(GPIOB, GPIO_PinSource11, GPIO_AF_I2C2);
+
+	/* SCL I2Cx_SCL_PIN */  /* SDA I2Cx_SDA_PIN*/
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_10 | GPIO_Pin_11;
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStruct.GPIO_OType = GPIO_OType_OD;
+	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+	I2C_InitStruct.I2C_Mode = I2C_Mode_I2C;
+	I2C_InitStruct.I2C_DutyCycle = I2C_DutyCycle_2;
+	I2C_InitStruct.I2C_OwnAddress1 = 0x00;
+	I2C_InitStruct.I2C_Ack = I2C_Ack_Enable;
+	I2C_InitStruct.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit;
+	I2C_InitStruct.I2C_ClockSpeed =100000;
+	I2C_Cmd(I2C2, ENABLE);
+	I2C_Init(I2C2, &I2C_InitStruct);
+
+}
+void i2c_Init()
+{
+	enable_i2c1();
+	enable_i2c2();
+}
