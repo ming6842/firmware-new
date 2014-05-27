9b67232a38f2bad6b8713ed36f2f3d5510b27c1a
fboris <fantasyboris@gmail.com>
2014-05-27 18:31:59 +0800

    First push of this world
    Former-commit-id: 9593f74287eaec76a4758c32023d007b685f3bbb [formerly b5fa4d1c9ec09f166ba1bea8f1f907192e821b1e]
    Former-commit-id: 57439be664e8f128192942ec8a781a32a4c1f5f6

--- program/test/mcu_periph/mpu9250.c
+++ program/test/mcu_periph/mpu9250.c
@@ -1,12 +1,27 @@
 #include "stm32f4xx_conf.h"
 #include "mpu9250.h"
 
+u8 SPI_xfer(SPI_TypeDef *SPIx, uint8_t  WriteByte)
+{
+	uint8_t rxdata;
+
+	SPI_I2S_SendData(SPIx, (uint16_t) WriteByte);
+	while (SPI_I2S_GetFlagStatus(SPIx, SPI_I2S_FLAG_TXE) == RESET);
+
+	while (SPI_I2S_GetFlagStatus(SPIx, SPI_FLAG_RXNE) == RESET);
+		rxdata = SPI_I2S_ReceiveData(SPIx);
+
+
+		return rxdata;
+}
+
 void mpu9250_read_who_am_i()
 {
 	uint8_t rxdata;
 	/*Keep CS Pin Low */
 	GPIO_ResetBits(GPIOE,GPIO_Pin_4);
 	Delay_1us(1);
+
 	SPI_I2S_SendData(SPI4, (uint16_t) 0xf5);
 	while (SPI_I2S_GetFlagStatus(SPI4, SPI_I2S_FLAG_TXE) == RESET);
 	
