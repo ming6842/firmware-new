0f0041ce83e69b5096f0843f273f634a94e0c1db
fboris <fantasyboris@gmail.com>
2014-05-27 21:47:32 +0800

    Fatal ERROR! Revert "change the routine more reasonable"
    This reverts commit 7d8d6dd1a2c092eb4c9a93be06d238a5f2e5eb5b [formerly 4aa80f8cc98f4bf3d77c80a9a3635b54a2026458] [formerly ad6a62b19ab888ad4c801160731807cd39e1e1af [formerly ce7375eb45878ef7b19d4e050304695714ef90cb]].
    
    
    Former-commit-id: 93d1e69926a00fd9509a84ba7f3e192bedeba362 [formerly 56fba6b0caadc6a0ba142beb179fca3907532795]
    Former-commit-id: 89bfa5a0c48c66a71579d5d745576dac2f76b7e6

--- program/test/ext_device/mpu9250.c
+++ program/test/ext_device/mpu9250.c
@@ -3,14 +3,15 @@
 
 #define MPU9250_CS_PIN_LOW GPIO_ResetBits(MPU9250_SPI_Port,MPU9250_SPI_CS_PIN)
 #define MPU9250_CS_PIN_HIGH GPIO_SetBits(MPU9250_SPI_Port,MPU9250_SPI_CS_PIN)
+
 void mpu9250_read_who_am_i()
 {
 	uint8_t rxdata;
 	MPU9250_CS_PIN_LOW;
 	Delay_1us(1);
-	
+	SPI_I2S_SendData(MPU6050_SPI_RCC_Periph, (uint16_t) 0xf5);
 	while (SPI_I2S_GetFlagStatus(MPU6050_SPI_RCC_Periph, SPI_I2S_FLAG_TXE) == RESET);
-		SPI_I2S_SendData(MPU6050_SPI_RCC_Periph, (uint16_t) 0xf5);
+	
 	while (SPI_I2S_GetFlagStatus(MPU6050_SPI_RCC_Periph, SPI_FLAG_RXNE) == RESET);
 		rxdata = SPI_I2S_ReceiveData(MPU6050_SPI_RCC_Periph);
 
