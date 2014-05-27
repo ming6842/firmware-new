6e199b89dc33094d6fd20cee065b247a15f2dff4
fboris <fantasyboris@gmail.com>
2014-05-27 21:24:05 +0800

    Cleaning while(1);

--- program/test/mcu_periph/test_periph.c
+++ program/test/mcu_periph/test_periph.c
@@ -26,41 +26,13 @@ int main(void)
 	uint8_t rxdata;
 	uint8_t i2c1_data;
 	while(1) {
-		I2C_GenerateSTART(I2C1, ENABLE);
-		//I2C_SendData(I2C1, (uint8_t) 0xA8);
-		/*wait EV5 */
-		while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_MODE_SELECT));
-		I2C_Send7bitAddress( I2C1, 0xAF, I2C_Direction_Transmitter);
-		/*Wait EV6*/
-		while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED));
-		while ( I2C_GetFlagStatus( I2C1, I2C_FLAG_TXE) == RESET );
-		
-		// I2C_SendData( I2C1, 0x00);
-		// while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED));
-		// while ( I2C_GetFlagStatus( I2C1, I2C_FLAG_TXE) == RESET );
-
-		// I2C_SendData( I2C1, 0x0A);
-		// while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED));
-		// while ( I2C_GetFlagStatus( I2C1, I2C_FLAG_TXE) == RESET );
-		// // while ( I2C_GetFlagStatus( I2C1, I2C_FLAG_TXE) == RESET );
-		I2C_GenerateSTOP(I2C1, ENABLE);
-		// /*Wait EV8_1*/
-		// while (!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTING));
-
-		// I2C_GenerateSTART(I2C2, ENABLE);
-		// I2C_SendData(I2C2, (uint8_t) 0xaa);
-		// while(I2C_GetFlagStatus(I2C2, I2C_FLAG_TXE) == RESET);
-		// I2C_GenerateSTOP(I2C2, ENABLE);
-
-		mpu9250_read_who_am_i();
 
 	Delay_1us(10);
 
 	GPIO_ResetBits(GPIOE,GPIO_Pin_4);
-	Delay_1us(1);
+	//Delay_1us(1);
 					 SPI_xfer(SPI4,0xf5);
 			rcv_id = SPI_xfer(SPI4,0xff);
-
 	GPIO_SetBits(GPIOE,GPIO_Pin_4);
 
 
@@ -83,13 +55,6 @@ int main(void)
 
 
 
-
-		SPI_I2S_SendData(SPI1, (uint16_t) 0xa5);
-		SPI_I2S_SendData(SPI1, (uint16_t) 0xa5);
-		SPI_I2S_SendData(SPI2, (uint16_t) 0xa5);
-		SPI_I2S_SendData(SPI2, (uint16_t) 0xa5);
-		// SPI_I2S_SendData(SPI4, (uint16_t) 0xa5);
-		// SPI_I2S_SendData(SPI4, (uint16_t) 0xa5);
 		GPIO_ToggleBits(GPIOE, GPIO_Pin_8 | GPIO_Pin_10 | GPIO_Pin_12 | GPIO_Pin_15);
 		USART_SendData( USART1, (uint16_t) 0xaa);
 		USART_SendData( USART2, (uint16_t) 0xaa);
