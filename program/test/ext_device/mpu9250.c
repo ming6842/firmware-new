ded4689943b251003176e0e9951fc773ebdea060
ming6842 <ming6842@hotmail.com>
2014-05-28 19:01:05 +0800

    Fixed array index error and added temperature reading
    Former-commit-id: f27d818f771063cb21f685955c42f2ecd437583b [formerly 48e328c257dbeafb44052b3e4bb477ec6bedfc9a]
    Former-commit-id: 3808bde245d88c956d6634c4ee543275cf943ee7

--- program/test/ext_device/mpu9250.c
+++ program/test/ext_device/mpu9250.c
@@ -83,15 +83,15 @@ void mpu9250_read_accel_temp_gyro(imu_unscaled_data_t* imu_unscaledData){
 		buffer[12] =  SPI_xfer(MPU9250_SPI,0xff);
 		buffer[13] =  SPI_xfer(MPU9250_SPI,0xff);
 
-		imu_unscaledData->acc[1] = ((uint16_t)buffer[0]<<8)+(uint16_t)buffer[1];
-		imu_unscaledData->acc[2] = ((uint16_t)buffer[2]<<8)+(uint16_t)buffer[3];
-		imu_unscaledData->acc[3] = ((uint16_t)buffer[4]<<8)+(uint16_t)buffer[5];
+		imu_unscaledData->acc[0] = ((uint16_t)buffer[0]<<8)|(uint16_t)buffer[1];
+		imu_unscaledData->acc[1] = ((uint16_t)buffer[2]<<8)|(uint16_t)buffer[3];
+		imu_unscaledData->acc[2] = ((uint16_t)buffer[4]<<8)|(uint16_t)buffer[5];
 
-		imu_unscaledData->temp = ((uint16_t)buffer[6]<<8)+(uint16_t)buffer[7];
+		imu_unscaledData->temp = ((uint16_t)buffer[6]<<8)|(uint16_t)buffer[7];
 
-		imu_unscaledData->gyro[1] = ((uint16_t)buffer[8]<<8)+(uint16_t)buffer[9];
-		imu_unscaledData->gyro[2] = ((uint16_t)buffer[10]<<8)+(uint16_t)buffer[11];
-		imu_unscaledData->gyro[3] = ((uint16_t)buffer[12]<<8)+(uint16_t)buffer[13];
+		imu_unscaledData->gyro[0] = ((uint16_t)buffer[8]<<8)|(uint16_t)buffer[9];
+		imu_unscaledData->gyro[1] = ((uint16_t)buffer[10]<<8)|(uint16_t)buffer[11];
+		imu_unscaledData->gyro[2] = ((uint16_t)buffer[12]<<8)|(uint16_t)buffer[13];
 
 
 	MPU9250_DESELECT();
@@ -101,15 +101,15 @@ void mpu9250_read_accel_temp_gyro(imu_unscaled_data_t* imu_unscaledData){
 
 void mpu9250_convert_to_scale(imu_unscaled_data_t* imu_unscaledData, imu_raw_data_t* imu_scaledData){
 
+	imu_scaledData->acc[0]	= (float)(imu_unscaledData->acc[0])*MPU9250A_8g;
 	imu_scaledData->acc[1]	= (float)(imu_unscaledData->acc[1])*MPU9250A_8g;
 	imu_scaledData->acc[2]	= (float)(imu_unscaledData->acc[2])*MPU9250A_8g;
-	imu_scaledData->acc[3]	= (float)(imu_unscaledData->acc[3])*MPU9250A_8g;
 
+	imu_scaledData->gyro[0]	= (float)(imu_unscaledData->gyro[0])*MPU9250G_1000dps;
 	imu_scaledData->gyro[1]	= (float)(imu_unscaledData->gyro[1])*MPU9250G_1000dps;
 	imu_scaledData->gyro[2]	= (float)(imu_unscaledData->gyro[2])*MPU9250G_1000dps;
-	imu_scaledData->gyro[3]	= (float)(imu_unscaledData->gyro[3])*MPU9250G_1000dps;
 
-
+	imu_scaledData->temp = ((float)(imu_unscaledData->temp)*MPU9250T_85degC+21.0); 
 
 
 }
