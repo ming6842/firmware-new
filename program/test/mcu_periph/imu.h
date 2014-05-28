b38b2d3420cd95eb75e62ad60679cc9cafb2ec35
ming6842 <ming6842@hotmail.com>
2014-05-28 11:32:16 +0800

    Adding MPU9250 Driver file
    Former-commit-id: 3a37b892c00f164fbab03e5754fe5ab527a335d4 [formerly d2a2c231531d1e4a54dbf5a9a3539164321ba983]
    Former-commit-id: 2f2504b57281bd9e4a28067f5f87c16b3af83792

---
+++ program/test/mcu_periph/imu.h
@@ -0,0 +1,19 @@
+#ifndef __IMU_H
+#define __IMU_H
+
+#include "mpu9250.h"
+
+#ifdef USE_IMU_MPU9250
+
+#define imu_update() mpu9250_read_accel_temp_gyro()
+
+#endif
+
+typedef struct imu_raw_data_t
+{
+	int16_t acc[3];
+	int16_t gyro[3];
+	uint16_t temp;
+}imu_raw_data_t;
+
+#endif
