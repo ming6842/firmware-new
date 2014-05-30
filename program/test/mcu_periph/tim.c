c779e7239562a20f06e7c696626955a39514fe23
fboris <fantasyboris@gmail.com>
2014-05-30 17:52:48 +0800

    Finish timer input capture for radio control signal
    Former-commit-id: 830a1b5dd98c88256eb95b366756b688144beacc [formerly dbf65f277667d133acaf5438c501edf6f38d3fea]
    Former-commit-id: cb322dee7d916673e7a07a04d17d4142634bfc03

--- program/test/mcu_periph/tim.c
+++ program/test/mcu_periph/tim.c
@@ -69,57 +69,71 @@ void enable_tim1(void)
 void enable_tim2(void)
 {
 
+	GPIO_InitTypeDef GPIO_InitStructure;
+	NVIC_InitTypeDef NVIC_InitStructure;
+	TIM_ICInitTypeDef  TIM_ICInitStructure;
+	TIM_TimeBaseInitTypeDef TIM_TimeBaseStruct;
+	
+	/* TIM4 clock enable */
 	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
 
-	/* -- GPIO Configuration ---------------------------------------------------- */
+	/* GPIOB clock enable */
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA | RCC_AHB1Periph_GPIOB, ENABLE);
+
+	/* TIM2 PWM3  PA0 */  /* TIM2 PWM4  PA1 */
+	GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_2 | GPIO_Pin_3 | GPIO_Pin_15;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_OD;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+	/* TIM4 chennel2 configuration : PB.07 */
+	GPIO_InitStructure.GPIO_Pin   = GPIO_Pin_3;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
+
+	/* Connect TIM pin to AF2 */
 	GPIO_PinAFConfig(GPIOA, GPIO_PinSource2, GPIO_AF_TIM2);
 	GPIO_PinAFConfig(GPIOA, GPIO_PinSource3, GPIO_AF_TIM2);
 	GPIO_PinAFConfig(GPIOA, GPIO_PinSource15, GPIO_AF_TIM2);
 	GPIO_PinAFConfig(GPIOB, GPIO_PinSource3, GPIO_AF_TIM2);
 
-	GPIO_InitTypeDef GPIO_InitStruct;
-	GPIO_InitStruct.GPIO_Pin =  GPIO_Pin_2 | GPIO_Pin_3 |
-				    GPIO_Pin_15;
-	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
-	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_100MHz;
-	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
-	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
+	/* Enable the TIM4 global Interrupt */
+	NVIC_InitStructure.NVIC_IRQChannel = TIM2_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&NVIC_InitStructure);
 
-	GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_3;
-	GPIO_Init(GPIOB, &GPIO_InitStruct);
-	/* -- Timer Configuration --------------------------------------------------- */
-	TIM_DeInit(TIM2);
-
-	TIM_TimeBaseInitTypeDef TIM_TimeBaseStruct;
-	TIM_TimeBaseStruct.TIM_Period = 2500 - 1;  //2.5ms , 400kHz
-	TIM_TimeBaseStruct.TIM_Prescaler = 84 - 1; //84 = 1M(1us)
-	TIM_TimeBaseStruct.TIM_ClockDivision = TIM_CKD_DIV1;
-	TIM_TimeBaseStruct.TIM_CounterMode = TIM_CounterMode_Up;
-
+	TIM_DeInit(TIM2);;
+	TIM_TimeBaseStruct.TIM_Period = 0xFFFF;              // 週期 = 2.5ms, 400kHz
+	TIM_TimeBaseStruct.TIM_Prescaler = 5;            // 除頻84 = 1M ( 1us )
+	TIM_TimeBaseStruct.TIM_ClockDivision = 0;
+	TIM_TimeBaseStruct.TIM_CounterMode = TIM_CounterMode_Up;    // 上數
 	TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStruct);
 
+	TIM_ICInitStructure.TIM_Channel = TIM_Channel_1;
+	TIM_ICInitStructure.TIM_ICPolarity = TIM_ICPolarity_Rising;
+	TIM_ICInitStructure.TIM_ICSelection = TIM_ICSelection_DirectTI;
+	TIM_ICInitStructure.TIM_ICPrescaler = TIM_ICPSC_DIV1;
+	TIM_ICInitStructure.TIM_ICFilter = 0x0;
 
-	/*TIM2 TIM3 TIM4 TIM8 */
-	TIM_OCInitTypeDef TIM_OCInitStruct;
-	TIM_OCInitStruct.TIM_OCMode = TIM_OCMode_PWM1;
-	TIM_OCInitStruct.TIM_OutputState = TIM_OutputState_Enable;
-	TIM_OCInitStruct.TIM_Pulse = 2000;
-	TIM_OCInitStruct.TIM_OCPolarity = TIM_OCPolarity_High;
+	TIM_ICInit(TIM2, &TIM_ICInitStructure);
 
-	TIM_OC1Init(TIM2, &TIM_OCInitStruct);
-	TIM_OC2Init(TIM2, &TIM_OCInitStruct);
-	TIM_OC3Init(TIM2, &TIM_OCInitStruct);
-	TIM_OC4Init(TIM2, &TIM_OCInitStruct);
-
-	TIM_OC1PreloadConfig(TIM2, TIM_OCPreload_Enable);
-	TIM_OC2PreloadConfig(TIM2, TIM_OCPreload_Enable);
-	TIM_OC3PreloadConfig(TIM2, TIM_OCPreload_Enable);
-	TIM_OC4PreloadConfig(TIM2, TIM_OCPreload_Enable);
-
-	TIM_ARRPreloadConfig(TIM2, ENABLE);
+	TIM_ICInitStructure.TIM_Channel = TIM_Channel_2;
+	TIM_ICInit(TIM2, &TIM_ICInitStructure);
+	TIM_ICInitStructure.TIM_Channel = TIM_Channel_3;
+	TIM_ICInit(TIM2, &TIM_ICInitStructure);
+	TIM_ICInitStructure.TIM_Channel = TIM_Channel_4;
+	TIM_ICInit(TIM2, &TIM_ICInitStructure);
+	/* TIM enable counter */
 	TIM_Cmd(TIM2, ENABLE);
+	/* Enable the CC2 Interrupt Request */
+	TIM_ITConfig(TIM2, TIM_IT_CC1, ENABLE);
+	TIM_ITConfig(TIM2, TIM_IT_CC2, ENABLE);
+	TIM_ITConfig(TIM2, TIM_IT_CC3, ENABLE);
+	TIM_ITConfig(TIM2, TIM_IT_CC4, ENABLE);
+
 }
 void enable_tim3(void)
 {
@@ -231,54 +245,59 @@ void enable_tim4(void)
 
 void enable_tim5(void)
 {
-
+	GPIO_InitTypeDef GPIO_InitStructure;
+	NVIC_InitTypeDef NVIC_InitStructure;
+	TIM_ICInitTypeDef  TIM_ICInitStructure;
+	TIM_TimeBaseInitTypeDef TIM_TimeBaseStruct;
+	/* TIM4 clock enable */
 	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM5, ENABLE);
 
-	/* -- GPIO Configuration ---------------------------------------------------- */
+	/* GPIOB clock enable */
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+
+	/* TIM2 PWM3  PA0 */  /* TIM2 PWM4  PA1 */
+	GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_0 | GPIO_Pin_1;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_OD;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+	/* Connect TIM pin to AF2 */
 	GPIO_PinAFConfig(GPIOA, GPIO_PinSource0, GPIO_AF_TIM5);
 	GPIO_PinAFConfig(GPIOA, GPIO_PinSource1, GPIO_AF_TIM5);
 
+	/* Enable the TIM4 global Interrupt */
+	NVIC_InitStructure.NVIC_IRQChannel = TIM5_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&NVIC_InitStructure);
 
-	GPIO_InitTypeDef GPIO_InitStruct;
-	GPIO_InitStruct.GPIO_Pin =  GPIO_Pin_0 | GPIO_Pin_1;
-	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
-	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_100MHz;
-	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
-	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
-
-	GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-	/* -- Timer Configuration --------------------------------------------------- */
 	TIM_DeInit(TIM5);
-
-	TIM_TimeBaseInitTypeDef TIM_TimeBaseStruct;
-	TIM_TimeBaseStruct.TIM_Period = 2500 - 1;  //2.5ms , 400kHz
-	TIM_TimeBaseStruct.TIM_Prescaler = 84 - 1; //84 = 1M(1us)
-	TIM_TimeBaseStruct.TIM_ClockDivision = TIM_CKD_DIV1;
-	TIM_TimeBaseStruct.TIM_CounterMode = TIM_CounterMode_Up;
-
+	TIM_TimeBaseStruct.TIM_Period = 0xFFFF;              // 週期 = 2.5ms, 400kHz
+	TIM_TimeBaseStruct.TIM_Prescaler = 5;            // 除頻84 = 1M ( 1us )
+	TIM_TimeBaseStruct.TIM_ClockDivision = 0;
+	TIM_TimeBaseStruct.TIM_CounterMode = TIM_CounterMode_Up;    // 上數
 	TIM_TimeBaseInit(TIM5, &TIM_TimeBaseStruct);
 
+	TIM_ICInitStructure.TIM_Channel = TIM_Channel_1;
+	TIM_ICInitStructure.TIM_ICPolarity = TIM_ICPolarity_Rising;
+	TIM_ICInitStructure.TIM_ICSelection = TIM_ICSelection_DirectTI;
+	TIM_ICInitStructure.TIM_ICPrescaler = TIM_ICPSC_DIV1;
+	TIM_ICInitStructure.TIM_ICFilter = 0x0;
 
-	/*TIM2 TIM3 TIM4 TIM8 */
-	TIM_OCInitTypeDef TIM_OCInitStruct;
-	TIM_OCInitStruct.TIM_OCMode = TIM_OCMode_PWM1;
-	TIM_OCInitStruct.TIM_OutputState = TIM_OutputState_Enable;
-	TIM_OCInitStruct.TIM_Pulse = 2000;
-	TIM_OCInitStruct.TIM_OCPolarity = TIM_OCPolarity_High;
+	TIM_ICInit(TIM5, &TIM_ICInitStructure);
 
-	TIM_OC1Init(TIM5, &TIM_OCInitStruct);
-	TIM_OC2Init(TIM5, &TIM_OCInitStruct);
-	TIM_OC3Init(TIM5, &TIM_OCInitStruct);
-	TIM_OC4Init(TIM5, &TIM_OCInitStruct);
+	TIM_ICInitStructure.TIM_Channel = TIM_Channel_2;
+	TIM_ICInit(TIM5, &TIM_ICInitStructure);
 
-	TIM_OC1PreloadConfig(TIM5, TIM_OCPreload_Enable);
-	TIM_OC2PreloadConfig(TIM5, TIM_OCPreload_Enable);
-	TIM_OC3PreloadConfig(TIM5, TIM_OCPreload_Enable);
-	TIM_OC4PreloadConfig(TIM5, TIM_OCPreload_Enable);
-
-	TIM_ARRPreloadConfig(TIM5, ENABLE);
+	/* TIM enable counter */
 	TIM_Cmd(TIM5, ENABLE);
+	/* Enable the CC2 Interrupt Request */
+	TIM_ITConfig(TIM5, TIM_IT_CC1, ENABLE);
+	TIM_ITConfig(TIM5, TIM_IT_CC2, ENABLE);
+	
 }
 void enable_tim9()
 {
