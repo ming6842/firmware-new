dd07fc38559e5c9e4143aa09708e6d86d5c54fd5
ming6842 <ming6842@hotmail.com>
2014-06-01 00:07:13 +0800

    Clean up tare_value part and get stuck in conditional compile USE_ADS1246_MPX6115A

--- program/test/estimator/vertical_estimator.c
+++ program/test/estimator/vertical_estimator.c
@@ -13,7 +13,13 @@ void vertical_estimator_init(vertical_data* raw_data,vertical_data* filtered_dat
 	filtered_data->Zdd=0.0;
 }
 
-void vertical_sense(void){
+void vertical_sense(vertical_data* vertical_filtered_data,vertical_data* vertical_raw_data,attitude_t* attitude){
 
+	/* Update barometer data */
+
+		if(!ADS1246_DRDY_PIN_STATE()){
+		vertical_raw_data->Z = barometer_read_altitude() ;
+		vertical_filtered_data->Z = lowpass_float(&vertical_filtered_data->Z, &vertical_raw_data->Z, 0.01f);
+		}
 
 }
