4f993cd15f5034867bb7ea501ce2b4c59ecce084
jackhsu98 <jackhsu98@gmail.com>
2014-06-02 16:24:44 +0800

    MPU6050 can read who_am_!

--- program/test/ext_device/AT24C04C.h
+++ program/test/ext_device/AT24C04C.h
@@ -1,4 +1,15 @@
 #ifndef __AT24C04C_H
 #define __AT24C04C_H
 void eeprom_byte_write(void);
+void eeprom_byte_read(void);
+
+
+
+/*MPU6050 I2C*/
+void I2C_start(I2C_TypeDef* I2Cx, uint8_t address, uint8_t direction);
+void I2C_write(I2C_TypeDef* I2Cx, uint8_t data);
+uint8_t I2C_read_ack(I2C_TypeDef* I2Cx);
+uint8_t I2C_read_nack(I2C_TypeDef* I2Cx);
+void I2C_stop(I2C_TypeDef* I2Cx);
+
 #endif
--- program/test/mcu_periph/test_periph.c
+++ program/test/mcu_periph/test_periph.c
@@ -1,6 +1,7 @@
 
 
 #define USE_IMU_MPU9250
+#define SLAVE_ADDRESS 0x68 << 1  // the slave address
 
 #include "stm32f4xx_conf.h"
 #include "gpio.h"
@@ -11,6 +12,7 @@
 #include "imu.h"
 #include <stdio.h>
 #include "attitude_estimator.h"
+#include "AT24C04C.h"
 
 
 
@@ -24,6 +26,8 @@ void Delay_1us(uint32_t nCnt_1us)
 int main(void)
 {
 	uint8_t buffer[100];
+	uint8_t received_data[1];
+	
 
 	imu_unscaled_data_t imu_unscaled_data;
 	imu_raw_data_t imu_raw_data;
@@ -41,7 +45,7 @@ int main(void)
 	usart_init();
 	spi_init();
 	pwm_input_output_init();
-	i2c_Init();
+	
 	usart2_dma_init();
 
 	//Delay_1us(2000000);
@@ -52,42 +56,58 @@ int main(void)
 	imu_calibrate_gyro_offset(&imu_offset,15000);
 	sprintf( (char *)buffer,"%d,%d,%d,\r\n",(int16_t)(imu_offset.gyro[0]),(int16_t)(imu_offset.gyro[1]),(int16_t)(imu_offset.gyro[2]));
 	usart2_dma_send(buffer);
+	
 
 	//Delay_1us(10000);
-
+	i2c_Init();
 	while(1) {
 
-		if(DMA_GetFlagStatus(DMA1_Stream6,DMA_FLAG_TCIF6)!=RESET){
-
-		 buffer[7]=0;
-		 buffer[8]=0;
-		 buffer[9]=0;
-		 buffer[10]=0;
-		 buffer[11]=0;
-		 buffer[12]=0;
-		 buffer[13]=0;
-		//sprintf(buffer,"%d,%d,%d,\r\n",(int16_t)(imu_raw_data.gyro[0]*10.0),(int16_t)(imu_raw_data.gyro[1]*10.0),(int16_t)(imu_raw_data.gyro[2]*10.0));
-		//sprintf(buffer,"%d,%d,%d,\r\n",(int16_t)(imu_raw_data.acc[0]*100.0),(int16_t)(imu_raw_data.acc[1]*100.0),(int16_t)(imu_raw_data.acc[2]*100.0));
-		//sprintf(buffer,"%d,%d,%d,\r\n",(int16_t)(lowpassed_acc_data.x*100.0),(int16_t)(lowpassed_acc_data.y*100.0),(int16_t)(lowpassed_acc_data.z*100.0));
-		//sprintf(buffer,"%d,%d,%d,\r\n",(int16_t)(predicted_g_data.x*100.0),(int16_t)(predicted_g_data.y*100.0),(int16_t)(predicted_g_data.z*100.0));
-		sprintf((char *)buffer,"%d,%d,%d,\r\n",
-			(int16_t) (attitude.roll*100.0f),
-			(int16_t) (attitude.pitch*100.0f),
-			(int16_t) (attitude.yaw*100.0f) );
+		//eeprom_byte_write();
+		//eeprom_byte_read();
 		
-		//sprintf(buffer,"%d,\r\n",(int16_t)(imu_raw_data.temp*100.0));
+		/*MPU6050 I2C*/
+		I2C_start(I2C1, SLAVE_ADDRESS, I2C_Direction_Transmitter); // start a transmission in Master transmitter mode
+        I2C_write(I2C1, 0x75); // write one byte to the slave
+        //I2C_write(I2C1, 0x00); // write another byte to the slave
+        I2C_stop(I2C1); // stop the transmission
+
+        I2C_start(I2C1, SLAVE_ADDRESS, I2C_Direction_Receiver); // start a transmission in Master receiver mode
+        //received_data[0] = I2C_read_ack(I2C1); // read one byte and request another byte
+        received_data[0] = I2C_read_nack(I2C1); // read one byte and don't request another byte, stop transmission
+		Delay_1us(1000);
+
+
+	// 	if(DMA_GetFlagStatus(DMA1_Stream6,DMA_FLAG_TCIF6)!=RESET){
+
+	// 	 buffer[7]=0;
+	// 	 buffer[8]=0;
+	// 	 buffer[9]=0;
+	// 	 buffer[10]=0;
+	// 	 buffer[11]=0;
+	// 	 buffer[12]=0;
+	// 	 buffer[13]=0;
+	// 	//sprintf(buffer,"%d,%d,%d,\r\n",(int16_t)(imu_raw_data.gyro[0]*10.0),(int16_t)(imu_raw_data.gyro[1]*10.0),(int16_t)(imu_raw_data.gyro[2]*10.0));
+	// 	//sprintf(buffer,"%d,%d,%d,\r\n",(int16_t)(imu_raw_data.acc[0]*100.0),(int16_t)(imu_raw_data.acc[1]*100.0),(int16_t)(imu_raw_data.acc[2]*100.0));
+	// 	//sprintf(buffer,"%d,%d,%d,\r\n",(int16_t)(lowpassed_acc_data.x*100.0),(int16_t)(lowpassed_acc_data.y*100.0),(int16_t)(lowpassed_acc_data.z*100.0));
+	// 	//sprintf(buffer,"%d,%d,%d,\r\n",(int16_t)(predicted_g_data.x*100.0),(int16_t)(predicted_g_data.y*100.0),(int16_t)(predicted_g_data.z*100.0));
+	// 	sprintf((char *)buffer,"%d,%d,%d,\r\n",
+	// 		(int16_t) (attitude.roll*100.0f),
+	// 		(int16_t) (attitude.pitch*100.0f),
+	// 		(int16_t) (attitude.yaw*100.0f) );
 		
-		usart2_dma_send(buffer);
+	// 	//sprintf(buffer,"%d,\r\n",(int16_t)(imu_raw_data.temp*100.0));
+		
+	// 	usart2_dma_send(buffer);
 
-		}
+	// 	}
 
 
-	imu_update(&imu_unscaled_data);
-	imu_scale_data(&imu_unscaled_data, &imu_raw_data,&imu_offset);
-	attitude_sense(&attitude,&imu_raw_data,&lowpassed_acc_data,&predicted_g_data);
+	// imu_update(&imu_unscaled_data);
+	// imu_scale_data(&imu_unscaled_data, &imu_raw_data,&imu_offset);
+	// attitude_sense(&attitude,&imu_raw_data,&lowpassed_acc_data,&predicted_g_data);
 
-		GPIO_ToggleBits(GPIOE, GPIO_Pin_8 | GPIO_Pin_10 | GPIO_Pin_12 | GPIO_Pin_15);
-//		Delay_1us(1000);
+	// 	GPIO_ToggleBits(GPIOE, GPIO_Pin_8 | GPIO_Pin_10 | GPIO_Pin_12 | GPIO_Pin_15);
+	// 	//Delay_1us(1000);
 	}
 
 	return 0;
