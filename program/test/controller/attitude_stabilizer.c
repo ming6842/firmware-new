9241ba8ed13f3369a842ef7901e45f4a0a04e37b
ming6842 <ming6842@hotmail.com>
2014-06-02 23:55:33 +0800

    Added PID for roll axis, worked

--- program/test/controller/attitude_stabilizer.c
+++ program/test/controller/attitude_stabilizer.c
@@ -6,7 +6,7 @@ void PID_roll(attitude_stablizer_pid_t* PID_control,imu_raw_data_t* imu_raw_data
 
 	float P = error * (PID_control -> kp);
 
-	float D = (imu_raw_data -> gyro[1]) * (PID_control -> kd);
+	float D = -(imu_raw_data -> gyro[0]) * (PID_control -> kd);
 
 	(PID_control -> output) = P+D;
  
@@ -15,8 +15,24 @@ void PID_roll(attitude_stablizer_pid_t* PID_control,imu_raw_data_t* imu_raw_data
 
 void PID_output(attitude_stablizer_pid_t* PID_roll,attitude_stablizer_pid_t* PID_pitch){
 
+motor_output_t motor;
 
+	motor. m1 =0.0;
+	motor. m2 =0.0;
+	motor. m3 =0.0;
+	motor. m4 =0.0;
+	motor. m5 =0.0;
+	motor. m6 =0.0;
+	motor. m7 =0.0;
+	motor. m8 =0.0;
+	motor. m9 =0.0;
+	motor. m10 =0.0;
+	motor. m11 =0.0;
+	motor. m12 =0.0;
 
-
-	
+	motor . m1 = - (PID_roll->output);
+	motor . m2 =   (PID_roll->output);
+	motor . m3 =   (PID_roll->output);
+	motor . m4 = - (PID_roll->output);
+	set_pwm_motor(&motor);
 }
