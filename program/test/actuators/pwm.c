ecb6935278361e2c10af0a1f3dc1add65e0a8061
fboris <fantasyboris@gmail.com>
2014-06-02 16:35:36 +0800

    move global struct to be static and rearrange pwm files
    Former-commit-id: b33c6d60e5e20f9f54db51332861f223e26260e4 [formerly c33e8c4ef527b6b6ab4e5d91a6ddcddc02e64284]
    Former-commit-id: c6a82ee44d842a97b582bafb075b4192f8b888e3

--- program/test/actuators/pwm.c
+++ program/test/actuators/pwm.c
@@ -3,21 +3,7 @@
 #include "bound.h"
 #include "delay.h"
 
-
-#define MOTOR1_PWM_PULSE TIM1->CCR1
-#define MOTOR2_PWM_PULSE TIM1->CCR2
-#define MOTOR3_PWM_PULSE TIM1->CCR3
-#define MOTOR4_PWM_PULSE TIM1->CCR4
-#define MOTOR5_PWM_PULSE TIM4->CCR1
-#define MOTOR6_PWM_PULSE TIM4->CCR2
-#define MOTOR7_PWM_PULSE TIM4->CCR3
-#define MOTOR8_PWM_PULSE TIM4->CCR4
-#define MOTOR9_PWM_PULSE TIM3->CCR1
-#define MOTOR10_PWM_PULSE TIM3->CCR2
-#define MOTOR11_PWM_PULSE TIM3->CCR3
-#define MOTOR12_PWM_PULSE TIM3->CCR4
-
-
+static pwm_motor_t motor[12];
 
 void init_pwm_motor(void)
 {
