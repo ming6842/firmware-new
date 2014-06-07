5f5305d0277b3b1adbc339730dd0dfa01eb4949b
fboris <fantasyboris@gmail.com>
2014-06-08 00:29:35 +0800

    Revert "Initial commit for testing new board"
    This reverts commit 8a9aa58d5b021f1156c8f7ea2d1e531ba665d46a.
    
    Conflicts:
    
    	program/Makefile
    	program/system/main.c

--- program/driver/serial.c
+++ program/driver/serial.c
@@ -14,30 +14,28 @@
   * @param  None
   * @retval None
   */
-void Serial_Config() /* Tx:Pb10, Rx:Pb11 */
+void Serial_Config(int buadrate) /* Tx:Pb10, Rx:Pb11 */
 {
 	/* RCC Initialization */
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2 |RCC_APB1Periph_USART3 | 
-		RCC_APB1Periph_UART4 | RCC_APB1Periph_UART5 | 
-		RCC_APB1Periph_UART8, ENABLE);
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB,  ENABLE);
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART3, ENABLE);
 
 	/* GPIO Initialization */
 	GPIO_InitTypeDef GPIO_InitStruct = {
-		.GPIO_Pin = GPIO_Pin_9 | GPIO_Pin_10,
+		.GPIO_Pin = GPIO_Pin_10 | GPIO_Pin_11,
 		.GPIO_Mode = GPIO_Mode_AF,
 		.GPIO_OType = GPIO_OType_PP,
 		.GPIO_PuPd = GPIO_PuPd_UP,
 		.GPIO_Speed = GPIO_Speed_50MHz
 	};
 
-	GPIO_PinAFConfig(GPIOA, GPIO_PinSource9, GPIO_AF_USART1);
-	GPIO_PinAFConfig(GPIOA, GPIO_PinSource10, GPIO_AF_USART1);
-	GPIO_Init(GPIOA, &GPIO_InitStruct);
+	GPIO_PinAFConfig(GPIOB, GPIO_PinSource10, GPIO_AF_USART3);
+	GPIO_PinAFConfig(GPIOB, GPIO_PinSource11, GPIO_AF_USART3);
+	GPIO_Init(GPIOB, &GPIO_InitStruct);
 
 	/* USART3 Initialization */
 	USART_InitTypeDef USART_InitStruct = {
-		.USART_BaudRate = 57600,
+		.USART_BaudRate = buadrate,
 		.USART_WordLength = USART_WordLength_8b,
 		.USART_StopBits = USART_StopBits_1,
 		.USART_Parity = USART_Parity_No,
@@ -45,137 +43,21 @@ void Serial_Config() /* Tx:Pb10, Rx:Pb11 */
 		.USART_Mode = USART_Mode_Rx | USART_Mode_Tx
 	};
 
-	USART_Init(USART1, &USART_InitStruct);
-	USART_Cmd(USART1, ENABLE);
-	/*USART2*/
-	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_5;
-	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
-	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
-	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
-	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
-
-	GPIO_PinAFConfig(GPIOD, GPIO_PinSource5, GPIO_AF_USART2);
-	GPIO_PinAFConfig(GPIOD, GPIO_PinSource6, GPIO_AF_USART2);
-	GPIO_Init(GPIOD, &GPIO_InitStruct);
-	
-	
-	USART_InitStruct.USART_BaudRate = 57600;
-	USART_InitStruct.USART_WordLength = USART_WordLength_8b;
-	USART_InitStruct.USART_StopBits = USART_StopBits_1;
-	USART_InitStruct.USART_Parity = USART_Parity_No;
-	USART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-	USART_InitStruct.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
-
-	USART_Init(USART2, &USART_InitStruct);
-	USART_Cmd(USART2, ENABLE);
-
-	/*USART3*/
-	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_9;
-	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
-	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
-	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
-	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
-
-	GPIO_PinAFConfig(GPIOD, GPIO_PinSource8, GPIO_AF_USART3);
-	GPIO_PinAFConfig(GPIOD, GPIO_PinSource9, GPIO_AF_USART3);
-	GPIO_Init(GPIOD, &GPIO_InitStruct);
-	
-	
-	USART_InitStruct.USART_BaudRate = 57600;
-	USART_InitStruct.USART_WordLength = USART_WordLength_8b;
-	USART_InitStruct.USART_StopBits = USART_StopBits_1;
-	USART_InitStruct.USART_Parity = USART_Parity_No;
-	USART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-	USART_InitStruct.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
-
 	USART_Init(USART3, &USART_InitStruct);
 	USART_Cmd(USART3, ENABLE);
+	USART_ClearFlag(USART3, USART_FLAG_TC);
 
+	/* NVIC Initialization */
+	USART_ITConfig(USART3, USART_IT_TXE, DISABLE);
+	USART_ITConfig(USART3, USART_IT_RXNE, ENABLE);
 
-	/*USART4*/
-	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_10 | GPIO_Pin_11;
-	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
-	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
-	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
-	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
-
-	GPIO_PinAFConfig(GPIOC, GPIO_PinSource10, GPIO_AF_UART4);
-	GPIO_PinAFConfig(GPIOC, GPIO_PinSource1, GPIO_AF_UART4);
-	GPIO_Init(GPIOC, &GPIO_InitStruct);
-	
-	
-	USART_InitStruct.USART_BaudRate = 57600;
-	USART_InitStruct.USART_WordLength = USART_WordLength_8b;
-	USART_InitStruct.USART_StopBits = USART_StopBits_1;
-	USART_InitStruct.USART_Parity = USART_Parity_No;
-	USART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-	USART_InitStruct.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
-
-	USART_Init(UART4, &USART_InitStruct);
-	USART_Cmd(UART4, ENABLE);
-
-	/*USART5*/
-	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_2 ;
-	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
-	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
-	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
-	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_PinAFConfig(GPIOD, GPIO_PinSource2, GPIO_AF_UART5);
-	GPIO_Init(GPIOD, &GPIO_InitStruct);
-
-	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_12;
-	GPIO_PinAFConfig(GPIOC, GPIO_PinSource12, GPIO_AF_UART4);
-	GPIO_Init(GPIOC, &GPIO_InitStruct);
-	
-	
-	USART_InitStruct.USART_BaudRate = 57600;
-	USART_InitStruct.USART_WordLength = USART_WordLength_8b;
-	USART_InitStruct.USART_StopBits = USART_StopBits_1;
-	USART_InitStruct.USART_Parity = USART_Parity_No;
-	USART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-	USART_InitStruct.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
-
-	USART_Init(UART5, &USART_InitStruct);
-	USART_Cmd(UART5, ENABLE);
-
-
-
-	/*USART8*/
-	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1;
-	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
-	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
-	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
-	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
-
-	GPIO_PinAFConfig(GPIOE, GPIO_PinSource0, GPIO_AF_UART8);
-	GPIO_PinAFConfig(GPIOE, GPIO_PinSource1, GPIO_AF_UART8);
-	GPIO_Init(GPIOE, &GPIO_InitStruct);
-	
-	
-	USART_InitStruct.USART_BaudRate = 57600;
-	USART_InitStruct.USART_WordLength = USART_WordLength_8b;
-	USART_InitStruct.USART_StopBits = USART_StopBits_1;
-	USART_InitStruct.USART_Parity = USART_Parity_No;
-	USART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-	USART_InitStruct.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
-
-	USART_Init(UART8, &USART_InitStruct);
-	USART_Cmd(UART8, ENABLE);
-
-
-	// USART_ClearFlag(USART3, USART_FLAG_TC);
-
-	// /* NVIC Initialization */
-	// USART_ITConfig(USART3, USART_IT_TXE, DISABLE);
-	// USART_ITConfig(USART3, USART_IT_RXNE, ENABLE);
-
-	// NVIC_InitTypeDef NVIC_InitStruct = {
-	// 	.NVIC_IRQChannel = USART3_IRQn,
-	// 	.NVIC_IRQChannelPreemptionPriority = configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY + 1,
-	// 	.NVIC_IRQChannelSubPriority = 0,
-	// 	.NVIC_IRQChannelCmd = ENABLE
-	// };
-	// NVIC_Init(&NVIC_InitStruct);
+	NVIC_InitTypeDef NVIC_InitStruct = {
+		.NVIC_IRQChannel = USART3_IRQn,
+		.NVIC_IRQChannelPreemptionPriority = configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY + 1,
+		.NVIC_IRQChannelSubPriority = 0,
+		.NVIC_IRQChannelCmd = ENABLE
+	};
+	NVIC_Init(&NVIC_InitStruct);
 }
 
 void USART3_IRQHandler()
--- program/driver/spi.c
+++ program/driver/spi.c
@@ -40,37 +40,3 @@ u8 SPI_ReadByte(SPI_TypeDef *SPIx)
 }
 /*=====================================================================================================*/
 /*=====================================================================================================*/
-void SPI_Config(void)
-{
-	GPIO_InitTypeDef GPIO_InitStruct;
-	SPI_InitTypeDef SPI_InitStruct;
-
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_SPI1, ENABLE);
-
-	GPIO_PinAFConfig(GPIOA, GPIO_PinSource4, GPIO_AF_SPI1);
-	GPIO_PinAFConfig(GPIOA, GPIO_PinSource5, GPIO_AF_SPI1);
-	GPIO_PinAFConfig(GPIOA, GPIO_PinSource6, GPIO_AF_SPI1);
-	GPIO_PinAFConfig(GPIOA, GPIO_PinSource7, GPIO_AF_SPI1);
-	/* CSN PB12 */
-	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_4 | GPIO_Pin_5 |
-					GPIO_Pin_6 | GPIO_Pin_7;
-	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
-	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
-	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
-	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_Init(GPIOA, &GPIO_InitStruct);
-	
-
-	SPI_InitStruct.SPI_Direction = SPI_Direction_2Lines_FullDuplex; // 雙線全雙工
-	SPI_InitStruct.SPI_Mode = SPI_Mode_Master; // 主模式
-	SPI_InitStruct.SPI_DataSize = SPI_DataSize_8b; // 數據大小8位
-	SPI_InitStruct.SPI_CPOL = SPI_CPOL_Low; // 時鐘極性，空閒時為低
-	SPI_InitStruct.SPI_CPHA = SPI_CPHA_1Edge; // 第1個邊沿有效，上升沿為采樣時刻
-	SPI_InitStruct.SPI_NSS = SPI_NSS_Soft; // NSS信號由軟件產生
-	SPI_InitStruct.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_2; // 8分頻，9MHz
-	SPI_InitStruct.SPI_FirstBit = SPI_FirstBit_MSB; // 高位在前
-	SPI_InitStruct.SPI_CRCPolynomial = 7;
-	SPI_Init(SPI1, &SPI_InitStruct);
-
-	SPI_Cmd(SPI1, ENABLE);
-}
