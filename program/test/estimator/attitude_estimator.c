c494fdde54970fac6d3eb32b65fb736e05598ab6
ming6842 <ming6842@hotmail.com>
2014-06-07 21:00:13 +0800

    Added delay before calibration, finished roll and pitch stabilization

--- program/test/estimator/attitude_estimator.c
+++ program/test/estimator/attitude_estimator.c
@@ -26,7 +26,7 @@ void attitude_estimator_init(attitude_t* attitude,imu_data_t* imu_raw_data, imu_
 void attitude_sense(attitude_t *attitude, imu_data_t *imu_raw_data, imu_data_t *imu_filtered_data, vector3d_t *True_R)
 {
 
-	float accel_lowpass_gain = 0.1f, gyro_lowpass_gain =0.03f,complementAlpha = 0.0001f;
+	float accel_lowpass_gain = 0.03f, gyro_lowpass_gain =0.03f,complementAlpha = 0.0001f;
 	float R_raw = 0.0f, inv_R_raw = 0.0f, R_true = 0.0f, inv_R_true = 0.0f;
 	float N_Ax_g = 0.0f, N_Ay_g = 0.0f, N_Az_g = 0.0f;
 
