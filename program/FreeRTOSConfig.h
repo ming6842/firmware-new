729b21a35ca09c2a7f5f05641dd370c471015406
Shengwen Cheng <shengwen1997.tw@gmail.com>
2014-06-11 00:06:58 +0800

    Fix the CPU clock rate

--- program/FreeRTOSConfig.h
+++ program/FreeRTOSConfig.h
@@ -84,7 +84,7 @@
 #define configUSE_PREEMPTION                    1
 #define configUSE_IDLE_HOOK                             1
 #define configUSE_TICK_HOOK                             0
-#define configCPU_CLOCK_HZ				(168000000)
+#define configCPU_CLOCK_HZ				(180000000)
 #define configTICK_RATE_HZ				( ( portTickType ) 1000 )
 #define configMAX_PRIORITIES			( ( unsigned portBASE_TYPE ) 10 )
 #define configMINIMAL_STACK_SIZE		( ( unsigned short ) 130 )
