00854d0da619aa9466d82e48522fe4276459b603
ming6842 <ming6842@hotmail.com>
2014-06-13 00:19:30 +0800

    Replace pointer with memcpy
    Former-commit-id: c1f80d7feb74c3e08273003a220f6a5496792d05 [formerly 49cb2ab29c90ae877665560b55ec12cc8957bb52]
    Former-commit-id: 42acf5b275b2a50ec68e7b48907d0af2c42591f8

--- program/ext_device/lea6h_ubx.c
+++ program/ext_device/lea6h_ubx.c
@@ -118,185 +118,48 @@ void UART4_IRQHandler(void)
 				chksumOKFlag=1;
 
 				if(msgID==0x12){
-					p=&UBXvelned.itow;
-					p[0]=ubxBuffer[6];
-					p[1]=ubxBuffer[7];
-					p[2]=ubxBuffer[8];
-					p[3]=ubxBuffer[9];
-			
-					p=&UBXvelned.velN;
-					p[0]=ubxBuffer[10];
-					p[1]=ubxBuffer[11];
-					p[2]=ubxBuffer[12];
-					p[3]=ubxBuffer[13];
-			
-					p=&UBXvelned.velE;
-					p[0]=ubxBuffer[14];
-					p[1]=ubxBuffer[15];
-					p[2]=ubxBuffer[16];
-					p[3]=ubxBuffer[17];
 
-					p=&UBXvelned.velD;
-					p[0]=ubxBuffer[18];
-					p[1]=ubxBuffer[19];
-					p[2]=ubxBuffer[20];
-					p[3]=ubxBuffer[21];
-
-					p=&UBXvelned.speed3D;
-					p[0]=ubxBuffer[22];
-					p[1]=ubxBuffer[23];
-					p[2]=ubxBuffer[24];
-					p[3]=ubxBuffer[25];
-
-					p=&UBXvelned.speed2D;
-					p[0]=ubxBuffer[26];
-					p[1]=ubxBuffer[27];
-					p[2]=ubxBuffer[28];
-					p[3]=ubxBuffer[29];
-
-					p=&UBXvelned.hdg2D;
-					p[0]=ubxBuffer[30];
-					p[1]=ubxBuffer[31];
-					p[2]=ubxBuffer[32];
-					p[3]=ubxBuffer[33];
-
-					p=&UBXvelned.speedAccu;
-					p[0]=ubxBuffer[34];
-					p[1]=ubxBuffer[35];
-					p[2]=ubxBuffer[36];
-					p[3]=ubxBuffer[37];
-
-					p=&UBXvelned.hdgAccu;
-					p[0]=ubxBuffer[38];
-					p[1]=ubxBuffer[39];
-					p[2]=ubxBuffer[40];
-					p[3]=ubxBuffer[41];
+					memcpy(&UBXvelned.itow,&ubxBuffer[6],sizeof(uint32_t));
+					memcpy(&UBXvelned.velN,&ubxBuffer[10],sizeof(uint32_t));
+					memcpy(&UBXvelned.velE,&ubxBuffer[14],sizeof(uint32_t));
+					memcpy(&UBXvelned.velD,&ubxBuffer[18],sizeof(uint32_t));
+					memcpy(&UBXvelned.speed3D,&ubxBuffer[22],sizeof(uint32_t));
+					memcpy(&UBXvelned.speed2D,&ubxBuffer[26],sizeof(uint32_t));
+					memcpy(&UBXvelned.hdg2D,&ubxBuffer[30],sizeof(uint32_t));
+					memcpy(&UBXvelned.speedAccu,&ubxBuffer[34],sizeof(uint32_t));
+					memcpy(&UBXvelned.hdgAccu,&ubxBuffer[38],sizeof(uint32_t));
 
 					UBXvelned.updatedFlag = 1;
 				}else if(msgID==0x06){
 
-					p=&UBXsol.itow;
-					p[0]=ubxBuffer[6];
-					p[1]=ubxBuffer[7];
-					p[2]=ubxBuffer[8];
-					p[3]=ubxBuffer[9];
-
-					p=&UBXsol.ftow;
-					p[0]=ubxBuffer[10];
-					p[1]=ubxBuffer[11];
-					p[2]=ubxBuffer[12];
-					p[3]=ubxBuffer[13];
-
-					p=&UBXsol.week;
-					p[0]=ubxBuffer[14];
-					p[1]=ubxBuffer[15];
-
-					p=&UBXsol.gpsFix;
-					p[0]=ubxBuffer[16];
-					
-					p=&UBXsol.statusFlag;
-					p[0]=ubxBuffer[17];
-
-					p=&UBXsol.ecefX;
-					p[0]=ubxBuffer[18];
-					p[1]=ubxBuffer[19];
-					p[2]=ubxBuffer[20];
-					p[3]=ubxBuffer[21];
-
-					p=&UBXsol.ecefY;
-					p[0]=ubxBuffer[22];
-					p[1]=ubxBuffer[23];
-					p[2]=ubxBuffer[24];
-					p[3]=ubxBuffer[25];
-
-					p=&UBXsol.ecefZ;
-					p[0]=ubxBuffer[26];
-					p[1]=ubxBuffer[27];
-					p[2]=ubxBuffer[28];
-					p[3]=ubxBuffer[29];
-
-					p=&UBXsol.pAcc;
-					p[0]=ubxBuffer[30];
-					p[1]=ubxBuffer[31];
-					p[2]=ubxBuffer[32];
-					p[3]=ubxBuffer[33];
-
-					p=&UBXsol.ecefVX;
-					p[0]=ubxBuffer[34];
-					p[1]=ubxBuffer[35];
-					p[2]=ubxBuffer[36];
-					p[3]=ubxBuffer[37];
-
-					p=&UBXsol.ecefVY;
-					p[0]=ubxBuffer[38];
-					p[1]=ubxBuffer[39];
-					p[2]=ubxBuffer[40];
-					p[3]=ubxBuffer[41];
-
-					p=&UBXsol.ecefVZ;
-					p[0]=ubxBuffer[42];
-					p[1]=ubxBuffer[43];
-					p[2]=ubxBuffer[44];
-					p[3]=ubxBuffer[45];
-
-					p=&UBXsol.vAcc;
-					p[0]=ubxBuffer[46];
-					p[1]=ubxBuffer[47];
-					p[2]=ubxBuffer[48];
-					p[3]=ubxBuffer[49];
-
-					p=&UBXsol.pDOP;
-					p[0]=ubxBuffer[50];
-					p[1]=ubxBuffer[51];
-
-					p=&UBXsol.numSV;
-					p[0]=ubxBuffer[53];
+					memcpy(&UBXsol.itow,&ubxBuffer[6],sizeof(uint32_t));
+					memcpy(&UBXsol.ftow,&ubxBuffer[10],sizeof(uint32_t));
+					memcpy(&UBXsol.week,&ubxBuffer[14],sizeof(uint16_t));
+					memcpy(&UBXsol.gpsFix,&ubxBuffer[16],sizeof(uint8_t));
+					memcpy(&UBXsol.statusFlag,&ubxBuffer[17],sizeof(uint8_t));
+					memcpy(&UBXsol.ecefX,&ubxBuffer[18],sizeof(uint32_t));
+					memcpy(&UBXsol.ecefY,&ubxBuffer[22],sizeof(uint32_t));
+					memcpy(&UBXsol.ecefZ,&ubxBuffer[26],sizeof(uint32_t));
+					memcpy(&UBXsol.ecefVX,&ubxBuffer[34],sizeof(uint32_t));
+					memcpy(&UBXsol.ecefVY,&ubxBuffer[38],sizeof(uint32_t));
+					memcpy(&UBXsol.ecefVZ,&ubxBuffer[42],sizeof(uint32_t));
+					memcpy(&UBXsol.vAcc,&ubxBuffer[46],sizeof(uint32_t));
+					memcpy(&UBXsol.pDOP,&ubxBuffer[46],sizeof(uint16_t));
+					memcpy(&UBXsol.numSV,&ubxBuffer[46],sizeof(uint8_t));
 
 
 					UBXsol.updatedFlag = 1;
 				}else if(msgID==0x02){
 
-					p=&UBXposLLH.itow;
-					p[0]=ubxBuffer[6];
-					p[1]=ubxBuffer[7];
-					p[2]=ubxBuffer[8];
-					p[3]=ubxBuffer[9];
 
-					p=&UBXposLLH.lon;
-					p[0]=ubxBuffer[10];
-					p[1]=ubxBuffer[11];
-					p[2]=ubxBuffer[12];
-					p[3]=ubxBuffer[13];
 
-					p=&UBXposLLH.lat;
-					p[0]=ubxBuffer[14];
-					p[1]=ubxBuffer[15];
-					p[2]=ubxBuffer[16];
-					p[3]=ubxBuffer[17];
-
-					p=&UBXposLLH.hEllipsoid;
-					p[0]=ubxBuffer[18];
-					p[1]=ubxBuffer[19];
-					p[2]=ubxBuffer[20];
-					p[3]=ubxBuffer[21];
-
-					p=&UBXposLLH.hMSL;
-					p[0]=ubxBuffer[22];
-					p[1]=ubxBuffer[23];
-					p[2]=ubxBuffer[24];
-					p[3]=ubxBuffer[25];
-
-					p=&UBXposLLH.hAcc;
-					p[0]=ubxBuffer[26];
-					p[1]=ubxBuffer[27];
-					p[2]=ubxBuffer[28];
-					p[3]=ubxBuffer[29];
-
-					p=&UBXposLLH.zAcc;
-					p[0]=ubxBuffer[30];
-					p[1]=ubxBuffer[31];
-					p[2]=ubxBuffer[32];
-					p[3]=ubxBuffer[33];
+					memcpy(&UBXposLLH.itow,&ubxBuffer[6],sizeof(uint32_t));
+					memcpy(&UBXposLLH.lon,&ubxBuffer[10],sizeof(uint32_t));
+					memcpy(&UBXposLLH.lat,&ubxBuffer[14],sizeof(uint32_t));
+					memcpy(&UBXposLLH.hEllipsoid,&ubxBuffer[18],sizeof(uint32_t));
+					memcpy(&UBXposLLH.hMSL,&ubxBuffer[22],sizeof(uint32_t));
+					memcpy(&UBXposLLH.hAcc,&ubxBuffer[26],sizeof(uint32_t));
+					memcpy(&UBXposLLH.zAcc,&ubxBuffer[30],sizeof(uint32_t));
 
 
 				LED_TOGGLE(LED2);
@@ -341,7 +204,6 @@ void UART4_IRQHandler(void)
 
 
 
-		LED_TOGGLE(LED1);
 	} 
 
 }
@@ -350,53 +212,22 @@ void lea6h_ubx_get_updated_data(UBXvelned_t *velned,UBXsol_t *sol, UBXposLLH_t *
 
 	if(UBXposLLH.updatedFlag==1){
 
-		posLLH -> itow 			= UBXposLLH.itow ;
-		posLLH -> lat 			= UBXposLLH.lat ;
-		posLLH -> lon 			= UBXposLLH.lon ;
-		posLLH -> hEllipsoid 	= UBXposLLH.hEllipsoid ;
-		posLLH -> hMSL 			= UBXposLLH.hMSL ;
-		posLLH -> hAcc 			= UBXposLLH.hAcc ;
-		posLLH -> zAcc 			= UBXposLLH.zAcc ;
-		posLLH -> updatedFlag 	= UBXposLLH.updatedFlag ;
+		memcpy(posLLH,&UBXposLLH,sizeof(UBXposLLH_t));
 
 		UBXposLLH.updatedFlag=0;
 	}
 
 	if(UBXsol.updatedFlag==1){
 
-		sol -> itow			= UBXsol.itow;
-		sol -> ftow			= UBXsol.ftow;
-		sol -> week			= UBXsol.week;
-		sol -> gpsFix		= UBXsol.gpsFix;
-		sol -> statusFlag	= UBXsol.statusFlag;
-		sol -> ecefX		= UBXsol.ecefX;
-		sol -> ecefY		= UBXsol.ecefY;
-		sol -> ecefZ		= UBXsol.ecefZ;
-		sol -> pAcc			= UBXsol.pAcc;
-		sol -> ecefVX		= UBXsol.ecefVX;
-		sol -> ecefVY		= UBXsol.ecefVY;
-		sol -> ecefVZ		= UBXsol.ecefVZ;
-		sol -> vAcc			= UBXsol.vAcc;
-		sol -> pDOP			= UBXsol.pDOP;
-		sol -> numSV		= UBXsol.numSV;
-		sol -> updatedFlag	= UBXsol.updatedFlag;
-	
+		memcpy(sol,&UBXsol,sizeof(UBXsol_t));
+
 		UBXsol.updatedFlag=0;
 	}
 
 
 	if(UBXvelned.updatedFlag==1){
 
-		velned -> itow			= UBXvelned.itow;
-		velned -> velN 			= UBXvelned.velN;
-		velned -> velE 			= UBXvelned.velE;
-		velned -> velD 			= UBXvelned.velD;
-		velned -> speed3D		= UBXvelned.speed3D;
-		velned -> speed2D		= UBXvelned.speed2D;
-		velned -> hdg2D			= UBXvelned.hdg2D;
-		velned -> speedAccu		= UBXvelned.speedAccu;
-		velned -> hdgAccu		= UBXvelned.hdgAccu;
-		velned -> updatedFlag	= UBXvelned.updatedFlag;
+		memcpy(velned,&UBXvelned,sizeof(UBXvelned_t));
 
 		UBXvelned.updatedFlag=0;
 	}
