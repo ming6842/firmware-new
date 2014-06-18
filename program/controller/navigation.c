c653a78d89ac51ce44d0b7e30dadd9b3b29a2396
ming6842 <ming6842@hotmail.com>
2014-06-19 00:59:43 +0800

    Finished nav algorithm, wating for test

--- program/controller/navigation.c
+++ program/controller/navigation.c
@@ -5,7 +5,7 @@
 // lat=N/S -> x, lon=E/W -> y
 
 
-void PID_Nav(nav_pid_t *PID_nav,attitude_t* attitude,UBXvelned_t * UBXvelned, UBXposLLH_t *UBXposLLH){
+void PID_Nav(nav_pid_t *PID_nav,attitude_t *attitude,UBXvelned_t *UBXvelned, UBXposLLH_t *UBXposLLH){
 
 	float S_heading= arm_sin_f32(attitude->yaw * (0.01745329252392f));
 	float C_heading= arm_cos_f32(attitude->yaw * (0.01745329252392f));
@@ -19,17 +19,30 @@ void PID_Nav(nav_pid_t *PID_nav,attitude_t* attitude,UBXvelned_t * UBXvelned, UB
 	float D_N = -(float)(UBXvelned -> velN) * (PID_nav -> kd);
 	float D_E = -(float)(UBXvelned -> velE) * (PID_nav -> kd);
 
-		
+
 	float P_roll_control = -P_lat*S_heading + P_lon*C_heading;
-	float P_pitch_control = 0.0 -P_lat*C_heading - P_lon*S_heading;
+	float P_pitch_control = 0.0f -P_lat*C_heading - P_lon*S_heading;
 
 	float D_roll_control = -D_N*S_heading + D_E*C_heading;
-	float D_pitch_control = 0.0 -D_N*C_heading - D_E*S_heading;
+	float D_pitch_control = 0.0f -D_N*C_heading - D_E*S_heading;
+
+
+	D_roll_control   = bound_float(D_roll_control,-40.0f,40.0f);
+	D_pitch_control  = bound_float(D_pitch_control ,-40.0f,40.0f);
+
+	P_roll_control   = bound_float(P_roll_control,-40.0f,40.0f);
+	P_pitch_control  = bound_float(P_pitch_control,-40.0f,40.0f);
+
+	(PID_nav -> output_roll) = P_roll_control+D_roll_control;
+	(PID_nav -> output_pitch) = P_pitch_control+D_pitch_control;
+
+	(PID_nav -> output_roll) = bound_float(PID_nav -> output_roll,-50.0f,50.0f);
+	(PID_nav -> output_pitch) = bound_float(PID_nav -> output_pitch,-50.0f,50.0f);
 
 } 
 
 
-/*
+/* Original code
 
 
 void PID_Nav_P_Only(){ 
