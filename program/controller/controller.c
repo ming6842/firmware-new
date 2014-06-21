f1e3c4205f86922d670bc47d96f8f55922be10f3
ming6842 <ming6842@hotmail.com>
2014-06-22 00:27:19 +0800

    Verified PID position vector
    Former-commit-id: fba74ad0c4c0d7392e76b98b39c9a737a99b70e6 [formerly b5897c99b30bf51f3b17df0eb035d59f0dccf869]
    Former-commit-id: 7c988078ad95737b47bed86d74f0ddaaa89b0a67

--- program/controller/controller.c
+++ program/controller/controller.c
@@ -90,8 +90,8 @@ void PID_init(attitude_stablizer_pid_t* PID_roll,attitude_stablizer_pid_t* PID_p
 	PID_Z -> setpoint =0.0;
 
 
-	PID_nav -> kp =0.0f;//0.045f;
-	PID_nav -> kd =0.5f;//0.06;
+	PID_nav -> kp =0.5f;//0.045f;
+	PID_nav -> kd =0.0f;//0.06;
 	PID_nav -> ki =0.0;
 	PID_nav -> out_max = +50.0f;
 	PID_nav -> out_min = -50.0f;
--- program/controller/navigation.c
+++ program/controller/navigation.c
@@ -5,39 +5,52 @@
 // lat=N/S -> x, lon=E/W -> y
 
 
-void PID_Nav(nav_pid_t *PID_nav,attitude_t *attitude,UBXvelned_t *UBXvelned, UBXposLLH_t *UBXposLLH){
+void PID_Nav(nav_pid_t *PID_control,attitude_t *attitude,UBXvelned_t *UBXvelned, UBXposLLH_t *UBXposLLH){
 
 	float S_heading= arm_sin_f32(attitude->yaw * (0.01745329252392f));
 	float C_heading= arm_cos_f32(attitude->yaw * (0.01745329252392f));
 
-	(PID_nav -> error.x) = (float)((PID_nav -> setpoint.x) -(UBXposLLH->lat));
-	(PID_nav -> error.y) = (float)((PID_nav -> setpoint.y) -(UBXposLLH->lon));
 
-	float P_lat = (PID_nav -> error.x)*(PID_nav -> kp);
-	float P_lon = (PID_nav -> error.y)*(PID_nav -> kp);
+	if( PID_control -> controller_status == CONTROLLER_ENABLE){
 
-	float D_N = -(float)(UBXvelned -> velN) * (PID_nav -> kd);
-	float D_E = -(float)(UBXvelned -> velE) * (PID_nav -> kd);
+		(PID_control -> error.x) = (float)((PID_control -> setpoint.x) -(UBXposLLH->lat));
+		(PID_control -> error.y) = (float)((PID_control -> setpoint.y) -(UBXposLLH->lon));
 
+		float P_lat = (PID_control -> error.x)*(PID_control -> kp);
+		float P_lon = (PID_control -> error.y)*(PID_control -> kp);
 
-	float P_roll_control = -P_lat*S_heading + P_lon*C_heading;
-	float P_pitch_control = 0.0f -P_lat*C_heading - P_lon*S_heading;
-
-	float D_roll_control = -D_N*S_heading + D_E*C_heading;
-	float D_pitch_control = 0.0f -D_N*C_heading - D_E*S_heading;
+		float D_N = -(float)(UBXvelned -> velN) * (PID_control -> kd);
+		float D_E = -(float)(UBXvelned -> velE) * (PID_control -> kd);
 
 
-	D_roll_control   = bound_float(D_roll_control,-40.0f,40.0f);
-	D_pitch_control  = bound_float(D_pitch_control ,-40.0f,40.0f);
+		float P_roll_control = -P_lat*S_heading + P_lon*C_heading;
+		float P_pitch_control = 0.0f -P_lat*C_heading - P_lon*S_heading;
 
-	P_roll_control   = bound_float(P_roll_control,-40.0f,40.0f);
-	P_pitch_control  = bound_float(P_pitch_control,-40.0f,40.0f);
+		float D_roll_control = -D_N*S_heading + D_E*C_heading;
+		float D_pitch_control = 0.0f -D_N*C_heading - D_E*S_heading;
 
-	(PID_nav -> output_roll) = P_roll_control+D_roll_control;
-	(PID_nav -> output_pitch) = P_pitch_control+D_pitch_control;
 
-	(PID_nav -> output_roll) = bound_float(PID_nav -> output_roll,-50.0f,50.0f);
-	(PID_nav -> output_pitch) = bound_float(PID_nav -> output_pitch,-50.0f,50.0f);
+		D_roll_control   = bound_float(D_roll_control,-40.0f,40.0f);
+		D_pitch_control  = bound_float(D_pitch_control ,-40.0f,40.0f);
+
+		P_roll_control   = bound_float(P_roll_control,-40.0f,40.0f);
+		P_pitch_control  = bound_float(P_pitch_control,-40.0f,40.0f);
+
+		(PID_control -> output_roll) = P_roll_control+D_roll_control;
+		(PID_control -> output_pitch) = P_pitch_control+D_pitch_control;
+
+		(PID_control -> output_roll) = bound_float(PID_control -> output_roll,-50.0f,50.0f);
+		(PID_control -> output_pitch) = bound_float(PID_control -> output_pitch,-50.0f,50.0f);
+	}else{
+
+		PID_control -> setpoint.x = UBXposLLH->lat;
+		PID_control -> setpoint.y = UBXposLLH->lon;
+
+		PID_control -> integral.x = 0.0f;
+		PID_control -> integral.y = 0.0f;
+		PID_control -> output_roll =0.0f;
+		PID_control -> output_pitch =0.0f;
+	}
 
 } 
 
@@ -45,7 +58,7 @@ void PID_Nav(nav_pid_t *PID_nav,attitude_t *attitude,UBXvelned_t *UBXvelned, UBX
 /* Original code
 
 
-void PID_Nav_P_Only(){ 
+void PID_control_P_Only(){ 
 S_heading = sin(MagHDG*0.01745329251994329576923690768489);
 C_heading = cos(MagHDG*0.01745329251994329576923690768489);
 Nav_lat_error = Nav_lat_setpoint - UBXposLLH.lat;
