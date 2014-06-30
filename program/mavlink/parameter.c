aac2d6876b9df22573cfc74f02fb0262df773203
Shengwen Cheng <shengwen1997.tw@gmail.com>
2014-06-30 10:25:00 +0800

    Try to implement the "parameter_read_single_value" handler

--- program/mavlink/parameter.c
+++ program/mavlink/parameter.c
@@ -25,11 +25,11 @@ void parameter_read_value(void)
 		if(get_global_data_access_right(i) == READ_WRITE) {
 			mavlink_msg_param_value_pack(
 				1, 0, &msg,
-				read_global_data_name(i),   	     /* Data name */ 
-				data_type ?  data_int : data_float,  /* Data value */
+				read_global_data_name(i),   	       /* Data name */ 
+				data_type ?  data_int : data_float,    /* Data value */
 				data_type ? MAV_PARAM_TYPE_INT16 : MAV_PARAM_TYPE_REAL32, /* Data type */
 				(uint16_t)get_modifiable_data_count(), /* Data count */
-				modifiable_data_cnt		     /* Index */
+				modifiable_data_cnt		       /* Index */
 			);
 			send_package(&msg);
 
@@ -43,6 +43,42 @@ void parameter_read_value(void)
 
 void parameter_read_single_value(void)
 {
+	mavlink_param_request_read_t mprr;
+	mavlink_msg_param_request_read_decode(&received_msg, &mprr);
+
+	Type data_type;
+	int data_int;
+	float data_float;
+
+	int i;
+	for(i = 0; i < get_global_data_count(); i++) {
+		if(get_global_data_access_right(i) == READ_ONLY)
+			continue;
+
+		if(strcmp(read_global_data_name(i), mprr.param_id) == 0) {
+			/* Prepare the data */
+			data_type = get_global_data_type(i);
+			if(data_type == INTEGER)
+				data_int = read_global_data_int(i);
+			else
+				data_float = read_global_data_float(i);
+			
+			mavlink_msg_param_value_pack(
+				1, 0, &msg,
+				read_global_data_name(i),   	       /* Data name */ 
+				data_type ?  data_int : data_float,    /* Data value */
+				data_type ? MAV_PARAM_TYPE_INT16 : MAV_PARAM_TYPE_REAL32, /* Data type */
+				(uint16_t)get_modifiable_data_count(), /* Data count */
+				mprr.param_index 		       /* Index */
+			);
+			send_package(&msg);
+
+			return;
+		}
+	}
+
+	/* Clear the received message */
+	received_msg.msgid = 0;
 }
 
 void parameter_write_value(void)
