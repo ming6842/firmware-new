158b7700238430b2c2fc8b5b871f69c31f477b51
Shengwen Cheng <shengwen1997.tw@gmail.com>
2014-07-10 15:59:38 +0800

    Modify the global.h data related definition as a new spec

--- program/mavlink/global.h
+++ program/mavlink/global.h
@@ -47,15 +47,32 @@ typedef enum {
 	READ_WRITE
 } AccessRight;
 
-/* Global data typedef */
-typedef enum { INTEGER, FLOAT } Type;
+/* Data type, map as the mavlink message type */
+typedef enum { 
+	UINT8  = 1,
+	INT8   = 2,
+	UINT16 = 3,
+	INT16  = 4,
+	UINT32 = 5,
+	INT32  = 6,
+	FLOAT  = 9,
+} Type;
+
+/* Multiple data union */
+typedef union {
+	uint8_t  uint8_value;
+	int8_t   int8_value;
+	uint16_t uint16_value;
+	int16_t  int16_value;
+	uint32_t uint32_value;
+	int32_t  int32_value;
+	float    float_value;
+} Data;
+
 typedef struct {
 	/* Data */
 	Type type;
-	union {
-		float flt_value;
-		int int_value;
-	};
+	Data data;
 
 	/* Data name */
 	char *name;
@@ -69,13 +86,11 @@ void init_global_data(void);
 int reset_global_data(int index, char *name, AccessRight access_right);
 int get_global_data_count(void);
 int get_modifiable_data_count(void);
-int set_global_data_int(int index, int value);
-int set_global_data_float(int index, float value);
+int set_global_data_value(int index, Type type, Data value);
 int get_global_data_type(int index, Type *type);
 int get_global_data_access_right(int index, AccessRight *access_right);
 int read_global_data_name(int index, char **name);
-int read_global_data_int(int index, int *value);
-int read_global_data_float(int index, float *value);
+int read_global_data_value(int index, Data *value);
 
 uint32_t get_boot_time(void);
 
