84be84856d9d5bc247fc0a88bc5702371a8d98f3
Shengwen Cheng <shengwen1997.tw@gmail.com>
2014-07-12 21:22:25 +0800

    Create the unit-test for global data

---
+++ program/test/global_data/main.c
@@ -0,0 +1,142 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include "global.h"
+
+#define PASSED 0
+#define FAIL   1
+
+/* Expect value table */
+#define TEST_INC_VALUE 10
+
+char *except_name[] = {
+	"test_uint8", "test_int8", "test_uint16", "test_int16",
+	"test_uint32", "test_int32", "test_float", "test_parameter_config"
+};
+
+int except_type[] = {UINT8, INT8, UINT16, INT16, UINT32, INT32, FLOAT, UINT8};
+char *type_name[] = {"X", "UINT8", "INT8", "UINT16", "INT16", "UINT32", "INT32",
+	"X", "X", "FLOAT", "UINT8"};
+
+Data expect_value[] = {
+	{.uint8_value = +8},
+	{.int8_value = -8},
+	{.uint16_value = +16},
+	{.int16_value = -16},
+	{.uint32_value = +32},
+	{.int32_value = -32},
+	{.float_value = 666.666},
+	{.uint8_value = 8}
+};
+
+bool expect_param_config[] = {false, false, false, false, false, false, false, true};
+
+/* Unit test */
+int main()
+{
+	int unit_test_status = PASSED;
+	printf("[Unit test for global data]\n");
+
+	/* Initialize the global data list */
+	init_global_data();
+
+	/* Name test */
+	printf("\[Name test]\n");
+	int i;
+	for(i = 0; i < get_global_data_count(); i++) {
+		char *testing_name;
+		read_global_data_name(i, &testing_name); /* Read the testing data */
+		printf("Testing name:%s | Expect name:%s", testing_name,
+			 *(except_name + i));
+
+		/* Compare the name */
+		if(strcmp(testing_name, *(except_name + i)) == 0) {
+			printf(" | Pass\n");
+		} else {
+			printf(" | Fail\n");
+			unit_test_status = FAIL;
+		}
+	}
+
+	/* Type test */
+	printf("\n[Data type test]\n");
+	for(i = 0; i < get_global_data_count(); i++) {
+		Type testing_type;
+		get_global_data_type(i, &testing_type);
+		printf("Testing type:%s | Expect type:%s", type_name[testing_type],
+			type_name[except_type[i]]);
+
+		/* Compare the type */
+		if(testing_type == except_type[i]) {
+			printf(" | Pass\n");
+		} else {
+			printf(" | Fail\n");
+			unit_test_status = FAIL;
+		}
+	}
+
+#if 0
+	/* Value test */
+	printf("\n[Value test]\n");
+	for(i = 0; i < get_global_data_count(); i++) {
+		Data testing_value;
+		read_global_data_value(i, DATA_POINTER_CAST(&testing_value));
+
+		switch(except_type[i]) {
+		    case UINT8:
+				printf("Testing value:%d | Expect Value:%d",
+					testing_value.uint8_value, expect_value[i].uint8_value);
+			break;
+		    case INT8:
+
+			break;	
+
+		    case UINT16:
+
+			break;	
+		    case INT16:
+
+			break;	
+		    case UINT32:
+
+			break;	
+		    case INT32:
+
+			break;	
+		    case FLOAT:
+
+			break;	
+		}
+
+		if(testing_value == expect_value[i])
+			printf(" | Pass\n");
+		} else {
+			printf(" | Fail\n");
+			unit_test_status = FAIL;
+		}
+	}
+#endif 
+
+	/* Parameter status flag test */
+	printf("\nParameter config status test\n");
+	for(i = 0; i < get_global_data_count(); i++) {
+		bool testing_param_config;
+		get_global_data_parameter_config_status(i, &testing_param_config);
+
+		printf("Testing bool status:%s | Expect bool status:%s",
+			testing_param_config ? "true" : "false",
+			expect_param_config[i] ? "true" : "false"
+		);
+
+		/* Compare the parameter config status */
+		if(testing_param_config == expect_param_config[i]) {
+			printf(" | Pass\n");
+		} else {
+			printf(" | Fail\n");
+			unit_test_status = FAIL;
+		}
+	}	
+
+	printf("\n\[Pass: %d, Fail:%d]\n", 0, 0);
+	return unit_test_status;
+}
