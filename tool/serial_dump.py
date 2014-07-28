127a3da0d453785bd9c711d738e20dfdc1876df1
fboris <fantasyboris@gmail.com>
2014-07-28 19:28:32 +0800

    Change command option, need to specify file name now

--- tool/serial_dump.py
+++ tool/serial_dump.py
@@ -11,26 +11,28 @@ if __name__ == '__main__':
 
 	port = "/dev/ttyUSB0"
 	baudrate = "57600"
-	second = 0.1
+	second = 0.001
 
 
-	if (len(sys.argv) < 3):
-		print("Usage: serial_dump.py /dev/ttyUSB0 57600")
+	if (len(sys.argv)  < 4 ):
+		print("Usage: \n./serial_dump.py /dev/ttyUSB0 57600 file_name 0.01")
 		exit()
-	elif (len(sys.argv) == 3):
-		port = sys.argv[1]
-		baudrate = sys.argv[2]
 	elif (len(sys.argv) == 4):
 		port = sys.argv[1]
 		baudrate = sys.argv[2]
-		second = float(sys.argv[3])
+		file_name = sys.argv[3]
+	elif (len(sys.argv) == 5):
+		port = sys.argv[1]
+		baudrate = sys.argv[2]
+		file_name = sys.argv[3]
+		second = float(sys.argv[4])
 	
 
 	print( "open {0}, buadrate {1}, delay in {2} seconds".format(port,baudrate,second))
 
 	ser = serial.Serial(port, baudrate);
 	
-	with open("gps.log","rb") as f:
+	with open(file_name,"rb") as f:
 		string = f.read()
 
 		for byte in string: 
