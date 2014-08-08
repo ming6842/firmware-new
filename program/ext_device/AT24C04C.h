bf3085290248a4dfb672e227ec2866895bc58713
Shengwen Cheng <shengwen1997.tw@gmail.com>
2014-08-08 14:19:26 +0800

    Create the eeprom abstract structure

--- program/ext_device/AT24C04C.h
+++ program/ext_device/AT24C04C.h
@@ -1,4 +1,20 @@
 #ifndef FILE_AT24C04C_H
 #define FILE_AT24C04C_H
-void eeprom_byte_write(void);
+
+/* EEPROM */
+typedef struct {
+	struct {
+		uint8_t page;
+		uint8_t page_offset; 
+	} _read;
+
+	struct {
+		uint8_t page;
+		uint8_t page_offset;
+	} _write;
+
+	void (*write)(uint8_t *buffer, int count);
+	void (*read)(uint8_t *data, int count);
+} eeprom_t;
+
 #endif
