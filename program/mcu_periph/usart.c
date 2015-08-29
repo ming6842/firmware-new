7fe5cd1bf1747803019457d70c8179723620d8c8
ming6842 <ming6842@hotmail.com>
2015-08-30 02:26:52 +0800

    clean up

--- program/mcu_periph/usart.c
+++ program/mcu_periph/usart.c
@@ -551,15 +551,12 @@ static void uartTX_stream_initialize(uart_streaming_fs_t* uart_fs){
 	uart_fs-> dmaISRTransmissionCompleteFlag = 0;
 	uart_fs-> dma_trigger_current_status = DMA_TRIGGER_STATUS_WaitingForData;
 
-	/* Memory 0 initialization, set this one to acitve first */
 	uart_fs-> dma_tx_buffer[0].currentIndex = 0;
 	uart_fs-> dma_tx_buffer[0].accessingFlag = 0;
 	uart_fs-> dma_tx_buffer[0].bufferAvailableSemRequestFlag = 0;
 	uart_fs-> dma_tx_buffer[0].waitCompleteSemRequestFlag = 0;
 	uart_fs-> dma_tx_buffer[0].DMATransmittingFlag = BUFFER_STATUS_BufferFilling;
 
-
-	/* Memory 1 initialization */
 	uart_fs-> dma_tx_buffer[1].currentIndex = 0;
 	uart_fs-> dma_tx_buffer[1].accessingFlag = 0;
 	uart_fs-> dma_tx_buffer[1].bufferAvailableSemRequestFlag = 0;
@@ -590,7 +587,6 @@ static ErrorMessage uartTX_stream_append_data_to_buffer(uart_streaming_fs_t* uar
 
 	ErrorMessage errorStatus = NO_ERROR;
 	uint8_t selected_buffer;
-	/* Check for active buffer to fill data into it */
 	if(uart_fs-> dma_tx_buffer[0].DMATransmittingFlag == BUFFER_STATUS_BufferFilling){
 
 		selected_buffer = 0;
@@ -600,38 +596,27 @@ static ErrorMessage uartTX_stream_append_data_to_buffer(uart_streaming_fs_t* uar
 		selected_buffer = 1;
 
 	}else {
-	/* No available buffer, return error */
 		errorStatus = NO_ACTIVE_BUFFER;
 		return errorStatus;
 	}
 
-	/* Check if busy flag is already set */
 	if(( uart_fs-> dma_tx_buffer[selected_buffer].accessingFlag & (uint32_t)(1<<task_id)) == 1){
 
 		errorStatus = PERMISSION_ALREADY_OCCUPIED;
 		return errorStatus;
 	}
 
-	/* Check if buffer size is enough for the packet */
 	if (len <(configUSART_DMA_TX_BUFFER_SIZE - uart_fs-> dma_tx_buffer[selected_buffer].currentIndex)){
 
-		/* Shift current index of buffer away to reserve space */
 		uint16_t startIndex = uart_fs-> dma_tx_buffer[selected_buffer].currentIndex;
 		uart_fs-> dma_tx_buffer[selected_buffer].currentIndex += len;
-
-		/* set occupy flag */
 		uart_fs-> dma_tx_buffer[selected_buffer].accessingFlag = uart_fs-> dma_tx_buffer[selected_buffer].accessingFlag | (uint32_t)(1<<task_id);
-
-		/* Fill in the buffer */
 		memcpy(&uart_fs-> dma_tx_buffer[selected_buffer].buffer[startIndex],s,len);
-
-		/* Reset occupy flag */
 		uart_fs-> dma_tx_buffer[selected_buffer].accessingFlag = uart_fs-> dma_tx_buffer[selected_buffer].accessingFlag & ~(uint32_t)(1<<task_id);
 
 
 	}else{
 
-		/* Not enough buffer space */
 		errorStatus = BUFFER_FULL;
 		return errorStatus;
 
@@ -643,12 +628,9 @@ static ErrorMessage uartTX_stream_append_data_to_buffer(uart_streaming_fs_t* uar
 static DMATriggerStatus uartTX_stream_dma_trigger(uart_streaming_fs_t* uart_fs){
 
 	uint8_t current_buffer;
-	/* Get current trigger condition */
-
 	switch(uart_fs-> dma_trigger_current_status ){
 		case DMA_TRIGGER_STATUS_WaitingForData:
 
-			/* Check for active buffer to fill data into it */
 			if(uart_fs-> dma_tx_buffer[0].DMATransmittingFlag == BUFFER_STATUS_BufferFilling){
 
 				current_buffer = 0;
@@ -659,13 +641,9 @@ static DMATriggerStatus uartTX_stream_dma_trigger(uart_streaming_fs_t* uart_fs){
 
 			}
 
-			/* check if there is any data inside buffer */
 			if(uart_fs-> dma_tx_buffer[current_buffer].currentIndex >0){
 
-				/* Close current buffer */
 				uart_fs-> dma_tx_buffer[current_buffer].DMATransmittingFlag = BUFFER_STATUS_ClosedWaitForTransmit;
-
-				/* Enable and reset counter for another buffer */
 				if(current_buffer == 0){
 					uart_fs-> dma_tx_buffer[1].DMATransmittingFlag = BUFFER_STATUS_BufferFilling;
 					uart_fs-> dma_tx_buffer[1].currentIndex = 0;
@@ -675,44 +653,33 @@ static DMATriggerStatus uartTX_stream_dma_trigger(uart_streaming_fs_t* uart_fs){
 					uart_fs-> dma_tx_buffer[0].currentIndex = 0;
 				}
 
-
-				/* Give semaphore to wake up the requested task (buffer available) */
 				int i;
 				for(i=0; i<DMA_TX_TaskID_COUNT ;i++){
 
 					 if((uart_fs-> dma_tx_buffer[!current_buffer].bufferAvailableSemRequestFlag & (uint32_t)(1<<i)) != 0){
 
-					 		/* Give semaphore */
+
 							xSemaphoreGive( uart_fs-> dma_tx_bufferAvailableSemaphore[i]);
 
 					 }
 
 				}
 
-				/* Clear the buffer available request sem flag */
 				uart_fs-> dma_tx_buffer[!current_buffer].bufferAvailableSemRequestFlag =0;
-
-				/* check for access flag */
 
 				if(uart_fs-> dma_tx_buffer[current_buffer].accessingFlag ==0){
 
-					/* Not busy, ready for transmit */
-
-					//////////////Set DMA////////////
 					uart_fs-> dma_send(uart_fs-> dma_tx_buffer[current_buffer].buffer,uart_fs-> dma_tx_buffer[current_buffer].currentIndex);
-					/* Set status flags */
 					uart_fs-> dma_tx_buffer[current_buffer].DMATransmittingFlag = BUFFER_STATUS_DMATransmitting;
 					uart_fs-> dma_trigger_current_status = DMA_TRIGGER_STATUS_WaitingForDMATransmissionComplete;
 				}else{
 
-					/* Some task is still filling in buffer, put trigger on pending */
 					uart_fs-> dma_trigger_current_status = DMA_TRIGGER_STATUS_WaitAccessFlag;
 				}
 
 
 			}else{
 
-				/* No data inside buffer, no need to transmit */
 
 			}
 
@@ -724,9 +691,6 @@ static DMATriggerStatus uartTX_stream_dma_trigger(uart_streaming_fs_t* uart_fs){
 
 			if( uart_fs-> dmaISRTransmissionCompleteFlag == 1){
 
-				/* Check which buffer was being transmitted */
-
-				/* Check which buffer is waiting to send */
 				if(uart_fs-> dma_tx_buffer[0].DMATransmittingFlag == BUFFER_STATUS_DMATransmitting){
 
 					current_buffer = 0;
@@ -737,25 +701,16 @@ static DMATriggerStatus uartTX_stream_dma_trigger(uart_streaming_fs_t* uart_fs){
 
 				}
 
-				/* clear the flag !! */
 				uart_fs-> dmaISRTransmissionCompleteFlag = 0;
-				/* Transmission is now complete, set the flag to idle */
 				uart_fs-> dma_tx_buffer[current_buffer].DMATransmittingFlag = BUFFER_STATUS_DMAIdle;
-
-				/* Record the transmitted bytes to log */
 				uart_fs-> total_transmitted_bytes += uart_fs-> dma_tx_buffer[current_buffer].currentIndex;
-
-				/* Set trigger state to send mode */
 				uart_fs-> dma_trigger_current_status = DMA_TRIGGER_STATUS_WaitingForData;
-
-				/* Give semaphore to wake up the requested task (send finished) */
 
 				int i;
 				for(i=0; i<DMA_TX_TaskID_COUNT ;i++){
 
 					 if((uart_fs-> dma_tx_buffer[current_buffer].waitCompleteSemRequestFlag & (uint32_t)(1<<i)) != 0){
 
-					 		/* Give semaphore */
 							xSemaphoreGive( uart_fs-> dma_tx_DMAWaitCompleteSemaphore [i]);
 
 					 }
@@ -763,15 +718,12 @@ static DMATriggerStatus uartTX_stream_dma_trigger(uart_streaming_fs_t* uart_fs){
 				}
 			}else{
 
-				/* Go and drink coffee dude ~ nothing to do now */
 			}
 
 		break;
 
 		case DMA_TRIGGER_STATUS_WaitAccessFlag:
 
-
-			/* Check which buffer is waiting to send */
 			if(uart_fs-> dma_tx_buffer[0].DMATransmittingFlag == BUFFER_STATUS_ClosedWaitForTransmit){
 
 				current_buffer = 0;
@@ -784,18 +736,10 @@ static DMATriggerStatus uartTX_stream_dma_trigger(uart_streaming_fs_t* uart_fs){
 
 			if(uart_fs-> dma_tx_buffer[current_buffer].accessingFlag == 0){
 
-						/* Not busy, ready for transmit */
-
-						//////////////Set DMA////////////
-						// usart2_dma_burst_send(buffer1,10);
-						
 						uart_fs-> dma_send(uart_fs-> dma_tx_buffer[current_buffer].buffer,uart_fs-> dma_tx_buffer[current_buffer].currentIndex);
-						/* Set status flags */
 						uart_fs-> dma_tx_buffer[current_buffer].DMATransmittingFlag = BUFFER_STATUS_DMATransmitting;
 						uart_fs-> dma_trigger_current_status = DMA_TRIGGER_STATUS_WaitingForDMATransmissionComplete;
 			}else{
-
-				/* keep waiting */
 
 			}
 
@@ -825,55 +769,36 @@ static DMATXTransmissionResult uartTX_stream_write(uart_streaming_fs_t* uart_fs,
 
 		}else if(err == BUFFER_FULL){
 
-			/* Handle buffer full according to user choice */
 			if(routineIfFailed == DMA_TX_FH_WaitReadySemaphore){
 
-			/* Set flag to request semaphore when buffer is available */
-				/* Check which buffer is being filled */
 				if(uart_fs-> dma_tx_buffer[0].DMATransmittingFlag == BUFFER_STATUS_BufferFilling){
 					req_buffer = 1;
-					/* set request flag into another buffer */
 					uart_fs-> dma_tx_buffer[1].bufferAvailableSemRequestFlag = uart_fs-> dma_tx_buffer[1].bufferAvailableSemRequestFlag | (uint32_t)(1<<task_id);
 
 
 				}else if(uart_fs-> dma_tx_buffer[1].DMATransmittingFlag == BUFFER_STATUS_BufferFilling){
 					req_buffer = 0;
-
-					/* set request flag into another buffer */
 					uart_fs-> dma_tx_buffer[0].bufferAvailableSemRequestFlag = uart_fs-> dma_tx_buffer[0].bufferAvailableSemRequestFlag | (uint32_t)(1<<task_id);
-
 				}
-
-
-				/* Wait semaphore */
 
 					if(xSemaphoreTake(uart_fs-> dma_tx_bufferAvailableSemaphore[task_id], (uint32_t)blockTime_ms/(1000.0 / configTICK_RATE_HZ)) == pdTRUE){
 
-						/* Just loop */
-
 					}else{
-
-						/* clear semaphore request flag */
 
 					uart_fs-> dma_tx_buffer[req_buffer].bufferAvailableSemRequestFlag = uart_fs-> dma_tx_buffer[req_buffer].bufferAvailableSemRequestFlag & ~(uint32_t)(1<<task_id);
 
-						/* quit */
 						shouldEnd = 1;
 						return DMA_TX_Result_TransmissionFailed;
 					}
 
-					/* Semaphore is taken now, let it roll again */
-
 			}else{
 
-				/* Just skip packet, exit now */
 				return DMA_TX_Result_TransmissionSkipped;
 
 			}
 
 		}else{
 
-			/* Transmission failed, routine error */
 			shouldEnd = 1;
 			transmissionResult = DMA_TX_Result_TransmissionFailed;
 		}
@@ -882,13 +807,8 @@ static DMATXTransmissionResult uartTX_stream_write(uart_streaming_fs_t* uart_fs,
 
 	}
 
-
-	/* Wait complete handler */
-
-	/* Check if data is appended to buffer */
 	if( transmissionResult == DMA_TX_Result_AppendedIntoBuffer){
 
-		/* Give semaphore to trigger the trigger task */
 		xSemaphoreGive(dma_tx_serviceWakeSemaphore);
 
 		if(waitcomplete == DMA_TX_TCH_NoWait){
@@ -901,26 +821,20 @@ static DMATXTransmissionResult uartTX_stream_write(uart_streaming_fs_t* uart_fs,
 
 			if(uart_fs-> dma_tx_buffer[0].DMATransmittingFlag == BUFFER_STATUS_BufferFilling){
 
-				/* set request flag into current buffer */
 				uart_fs-> dma_tx_buffer[0].waitCompleteSemRequestFlag = uart_fs-> dma_tx_buffer[0].waitCompleteSemRequestFlag | (uint32_t)(1<<task_id);
 
 
 			}else if(uart_fs-> dma_tx_buffer[1].DMATransmittingFlag == BUFFER_STATUS_BufferFilling){
 
-				/* set request flag into current buffer */
 				uart_fs-> dma_tx_buffer[1].waitCompleteSemRequestFlag = uart_fs-> dma_tx_buffer[1].waitCompleteSemRequestFlag | (uint32_t)(1<<task_id);
 
 			}
-
-			/* Wait transmission complete semaphore */
 
 				if(xSemaphoreTake(uart_fs-> dma_tx_DMAWaitCompleteSemaphore [task_id], (uint32_t)blockTime_ms/(1000.0 / configTICK_RATE_HZ)) == pdTRUE){
 
 						return DMA_TX_Result_TransmissionComplete;
 
 				}else{
-
-						/* clear semaphore request flag */
 
 				uart_fs-> dma_tx_buffer[0].waitCompleteSemRequestFlag = uart_fs-> dma_tx_buffer[0].waitCompleteSemRequestFlag & ~(uint32_t)(1<<task_id);
 
@@ -931,7 +845,6 @@ static DMATXTransmissionResult uartTX_stream_write(uart_streaming_fs_t* uart_fs,
 
 
 		}else {
-			/* Should not come here */
 
 			return 0x99;
 		}
